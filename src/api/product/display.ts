203ec4fe3fe6b05938bf2d4c166eae4ec613f81c
George <george@geekstudio.co.kr>
2022-05-03 13:52:04 +0900

    :sparkles: feature: shopAPI > product > Display API  추가
    get display brands 브랜드 목록 조회하기
    get display brands {brandNo} 브랜드 상세 조회하기

---
+++ src/api/product/display.ts
@@ -0,0 +1,35 @@
+import { AxiosResponse } from 'axios';
+
+import request from 'api/core';
+
+interface BrandsParams {
+    filter?: { name: String; categoryNo: Number };
+    pageNumber?: Number;
+    pageSize?: Number;
+    hasTotalCount?: Boolean;
+    fromDB?: Boolean;
+    sort?: { criterion: String; direction: String };
+}
+
+const display = {
+    getDisplayBrands: ({
+        filter,
+        pageNumber,
+        pageSize,
+        hasTotalCount,
+        fromDB,
+        sort,
+    }: BrandsParams): Promise<AxiosResponse> =>
+        request({
+            method: 'GET',
+            url: '/display/brands',
+        }),
+
+    getDisplayBrandsDetail: (brandNo: String): Promise<AxiosResponse> =>
+        request({
+            method: 'GET',
+            url: `/display/brands/${brandNo}`,
+        }),
+};
+
+export default display;
