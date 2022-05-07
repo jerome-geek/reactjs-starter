e058fd9c6dc4daede9f6a5d90c34f69d44ad8c5a
Jerome <jerome@geekstudio.co.kr>
2022-05-07 12:07:46 +0900

    :sparkles: feat: Axios Instance 수정

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -1,20 +1,32 @@
 import axios, { AxiosInstance } from 'axios';
 
-// Axios Instance: https://pinokio0702.tistory.com/373 참고
-const request: AxiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    headers: {
+import { getPlatform } from 'utils';
+
+export const defaultHeaders = () => {
+    return {
         version: process.env.REACT_APP_VERSION || '',
         clientId: process.env.REACT_APP_CLIENT_ID || '',
-        platform: 'PC',
-    },
+        platform: getPlatform(),
+    };
+};
+
+const request: AxiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+    headers: defaultHeaders(),
 });
 
 // 요청 타임아웃 설정
 request.defaults.timeout = 2500;
+
 // 요청 인터셉터 추가
-request.interceptors.request.use();
+request.interceptors.request.use((config) => {
+    return config;
+});
+
 // 응답 인터셉터 추가
-request.interceptors.response.use();
+request.interceptors.response.use((res) => {
+    // TODO: 토큰이 만료됐을 경우 처리 필요
+    return res;
+});
 
 export default request;
