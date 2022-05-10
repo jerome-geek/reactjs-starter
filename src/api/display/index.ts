d3e770a3dd82e4ab89072b92e463a4ffcdbc9b97
Jerome <jerome@geekstudio.co.kr>
2022-05-10 11:06:47 +0900

    :sparkles: feat: shopAPI > display > review API 추가

--- src/api/display/index.ts
+++ src/api/display/index.ts
@@ -1,4 +1,5 @@
 import banner from 'api/display/banner';
 import category from 'api/display/category';
+import review from 'api/display/review';
 
-export { banner, category };
+export { banner, category, review };
