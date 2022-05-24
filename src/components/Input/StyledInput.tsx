6bd2492173e112b9149bc6600f8f661369d5054a
Jerome <jerome@geekstudio.co.kr>
2022-05-24 15:28:36 +0900

    :sparkles: feat: 로그인 기능 추가

---
+++ src/components/Input/StyledInput.tsx
@@ -0,0 +1,29 @@
+import { InputHTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    border?: string;
+    borderRadius?: string;
+    padding?: string;
+    fontSize?: string;
+}
+
+const StyledInput = styled.input<InputProps>`
+    border: ${(props) => props.border || '1px solid black'};
+    border-radius: ${(props) => props.borderRadius};
+    outline: none;
+    font-size: 20px;
+    font-weight: 600;
+    min-height: 50px;
+
+    &::placeholder {
+        font-size: ${(props) => props.fontSize || '16px'};
+        color: ${(props) => props.theme.gray3};
+    }
+
+    &:focus {
+        border: 1px solid red;
+    }
+`;
+
+export default StyledInput;
