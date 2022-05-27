cf241e8636dac67be983f9516985e28e430d1f4b
George <george@geekstudio.co.kr>
2022-05-27 12:40:19 +0900

    :sparkles: feat: shop API > order > orderSheet, purchase, naverPay API 작성

---
+++ src/api/order/orderSheet.ts
@@ -0,0 +1,146 @@
+import { AxiosResponse } from 'axios';
+
+import request, { defaultHeaders } from 'api/core';
+import {
+    OrderSheetBody,
+    GetCalculatedOrderSheet,
+    CouponRequest,
+} from 'models/order/index';
+
+const orderSheet = {
+    // TODO 400 error, message: "구매불가한 옵션이 포함되어 있습니다."
+    writeOrderSheet: ({
+        productCoupons,
+        trackingKey,
+        cartNos,
+        channelType,
+        products,
+    }: OrderSheetBody): Promise<AxiosResponse> =>
+        request({
+            method: 'POST',
+            url: '/order-sheets',
+            data: {
+                productCoupons,
+                trackingKey,
+                cartNos,
+                channelType,
+                products,
+            },
+            headers: Object.assign({}, defaultHeaders(), {
+                accessToken: localStorage.getItem('accessToken') || '',
+            }),
+        }),
+
+    // TODO 404 error, message: "잘못된 요청입니다.", 이하 함수들 전부 orderSheetNo를 필수로 요구하므로 나중에 테스트 必
+    getOrderSheet: (
+        orderSheetNo: string,
+        { includeMemberAddress }: { includeMemberAddress: boolean },
+    ): Promise<AxiosResponse> =>
+        request({
+            method: 'GET',
+            url: `/order-sheets/${orderSheetNo}`,
+            params: { includeMemberAddress },
+            headers: Object.assign({}, defaultHeaders(), {
+                accessToken: localStorage.getItem('accessToken') || '',
+            }),
+        }),
+
+    getCalculatedOrderSheet: (
+        orderSheetNo: string,
+        {
+            addressRequest,
+            couponRequest,
+            accumulationUseAmt,
+            shippingAddress,
+        }: GetCalculatedOrderSheet,
+    ): Promise<AxiosResponse> =>
+        request({
+            method: 'POST',
+            url: `/order-sheets/${orderSheetNo}`,
+            data: {
+                addressRequest,
+                couponRequest,
+                accumulationUseAmt,
+                shippingAddress,
+            },
+            headers: Object.assign({}, defaultHeaders(), {
+                accessToken: localStorage.getItem('accessToken') || '',
+            }),
+        }),
+
+    getCanApplyCoupon: (
+        orderSheetNo: string,
+        { channelType }: { channelType: string },
+    ): Promise<AxiosResponse> =>
+        request({
+            method: 'GET',
+            url: `/order-sheets/${orderSheetNo}/coupons`,
+            params: { channelType },
+            headers: Object.assign({}, defaultHeaders(), {
+                accessToken: localStorage.getItem('accessToken') || '',
+            }),
+        }),
+
+    applyCoupon: (
+        orderSheetNo: string,
+        {
+            cartCouponIssueNo,
+            channelType,
+            promotionCode,
+            productCoupons,
+        }: CouponRequest,
+    ): Promise<AxiosResponse> =>
+        request({
+            method: 'POST',
+            url: `/order-sheets/${orderSheetNo}/coupons/apply`,
+            data: {
+                cartCouponIssueNo,
+                channelType,
+                promotionCode,
+                productCoupons,
+            },
+            headers: Object.assign({}, defaultHeaders(), {
+                accessToken: localStorage.getItem('accessToken') || '',
+            }),
+        }),
+
+    appliedCouponPrice: (
+        orderSheetNo: string,
+        {
+            cartCouponIssueNo,
+            channelType,
+            promotionCode,
+            productCoupons,
+        }: CouponRequest,
+    ): Promise<AxiosResponse> =>
+        request({
+            method: 'POST',
+            url: `/order-sheets/${orderSheetNo}/coupons/calculate`,
+            data: {
+                cartCouponIssueNo,
+                channelType,
+                promotionCode,
+                productCoupons,
+            },
+            headers: Object.assign({}, defaultHeaders(), {
+                accessToken: localStorage.getItem('accessToken') || '',
+            }),
+        }),
+
+    maximumAppliedCouponPrice: (
+        orderSheetNo: string,
+        { channelType }: { channelType?: string },
+    ): Promise<AxiosResponse> =>
+        request({
+            method: 'POST',
+            url: `/order-sheets/${orderSheetNo}/coupons/calculate`,
+            data: {
+                channelType,
+            },
+            headers: Object.assign({}, defaultHeaders(), {
+                accessToken: localStorage.getItem('accessToken') || '',
+            }),
+        }),
+};
+
+export default orderSheet;
