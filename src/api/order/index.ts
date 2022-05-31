cfd82ba715e8b882af2bd5b6a7f1c72c99bb0865
George <george@geekstudio.co.kr>
2022-05-31 10:20:49 +0900

    :sparkles: feat: shop API > order > myOrder API

--- src/api/order/index.ts
+++ src/api/order/index.ts
@@ -4,5 +4,14 @@ import orderConfiguration from 'api/order/orderConfiguration';
 import orderSheet from 'api/order/orderSheet';
 import purchase from 'api/order/purchase';
 import naverPay from 'api/order/naverPay';
+import myOrder from 'api/order/myOrder';
 
-export { cart, guestOrder, orderConfiguration, orderSheet, purchase, naverPay  };
+export {
+    cart,
+    guestOrder,
+    orderConfiguration,
+    orderSheet,
+    purchase,
+    naverPay,
+    myOrder,
+};
