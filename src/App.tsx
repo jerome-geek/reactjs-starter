6ab9e808047f5da7d7eea0ba78b81302d1cf65ab
jerome <jerome@geekstudio.co.kr>
2022-06-07 15:20:20 +0900

    :sparkles: feat: PrivateRoute 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,56 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { useTranslation } from 'react-i18next';
 
+import MyPageRouter from 'router/MyPageRouter';
+import MemberRouter from 'router/MemberRouter';
 import Main from 'pages/Main';
-import Login from 'pages/Member/Login';
 import NotFound from 'pages/NotFound';
 import Loader from 'components/shared/Loader';
-import Header from 'components/shared/Header';
 import Footer from 'components/shared/Footer';
-import Login from 'pages/Member/Login';
 import SignUpTerms from 'pages/Member/SignUpTerms';
 import SignUpInput from 'pages/Member/SignUpInput';
-import mall from 'api/mall';
-import { useMall } from 'hooks';
+import { useMall, useMember } from 'hooks';
+import EventDetail from 'pages/Event/EventDetail';
+import PrivateRoute from 'hoc/PrivateRoute';
 
 const App: FC = () => {
     const [data, isLoading] = useMall();
-
-    const { t } = useTranslation('main');
 
     return (
         <>
             {isLoading ? (
                 <Loader />
             ) : (
-                <>
-                    <BrowserRouter>
-                        <Header />
-                        {t('hello')}
-                        <Routes>
-                            <Route path='/' element={<Main />} />
-                            <Route path='/member/login' element={<Login />} />
-                            <Route path='/*' element={<NotFound />} />
-                            <Route
-                                path='/signup/term'
-                                element={<SignUpTerms />}
-                            />
-                            <Route
-                                path='/signup/input'
-                                element={<SignUpInput />}
-                            />
-                        </Routes>
-                        <Footer />
-                    </BrowserRouter>
-                </>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<Main />} />
+                        <Route
+                            path='/member/*'
+                            element={
+                                <PrivateRoute option={false}>
+                                    <MemberRouter />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path='/my-page/*'
+                            element={
+                                <PrivateRoute option={true}>
+                                    <MyPageRouter />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path='/event/:eventNo'
+                            element={<EventDetail />}
+                        />
+                        <Route path='/signup/term' element={<SignUpTerms />} />
+                        <Route path='/signup/input' element={<SignUpInput />} />
+                        <Route path='/*' element={<NotFound />} />
+                    </Routes>
+                    <Footer />
+                </BrowserRouter>
             )}
             <ReactQueryDevtools initialIsOpen={false} />
         </>
