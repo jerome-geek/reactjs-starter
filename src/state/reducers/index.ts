4f4b0e7dee3165feb9c472cfac1ba510175b96f8
jerome <jerome@geekstudio.co.kr>
2022-06-10 10:52:31 +0900

    :sparkles: feat: dispatch 재작성 & memberSlice에 thunk 추가

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
 
+import store from 'state/store';
 import mallSlice from 'state/slices/mallSlice';
 import tokenSlice from 'state/slices/tokenSlice';
 import memberSlice from 'state/slices/memberSlice';
@@ -11,9 +12,11 @@ const rootReducer = combineReducers({
     member: memberSlice.reducer,
 });
 
-export default rootReducer;
-
 export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>(); // Export a hook that can be reused to resolve types
 
 // useSelector hook 대신 사용. useSelector 함수의 파라미터에 타입을 지정하지 않아도 된다.
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default rootReducer;
--- src/state/slices/memberSlice.ts
+++ src/state/slices/memberSlice.ts
@@ -1,20 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { profile } from 'api/member';
+import { ProfileBody } from 'models/member';
 
-const initialState: any = {};
+export const fetchProfile = createAsyncThunk('member/profile', async () => {
+    try {
+        const response = await profile.getProfile();
+        return response.data;
+    } catch (error) {
+        return error;
+    }
+});
+
+const memberInitialState: {
+    loading: boolean;
+    data: Nullable<ProfileBody>;
+    error: any;
+} = {
+    loading: false,
+    data: null,
+    error: {},
+};
 
 export const memberSlice = createSlice({
     name: 'member',
-    initialState,
-    reducers: {
-        getProfile: (state, action) => {
-            return { ...state, ...action.payload };
-        },
-        setProfile: (state, action) => {
-            return { ...state, ...action.payload };
-        },
+    initialState: memberInitialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchProfile.pending, (state, action) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchProfile.fulfilled, (state, action) => {
+            state.loading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchProfile.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error;
+        });
     },
 });
-
-export const { getProfile, setProfile } = memberSlice.actions;
 
 export default memberSlice;
