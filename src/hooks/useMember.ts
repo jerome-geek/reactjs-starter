87e4e42edcc5bff66581ba714695dd1e254f8bdc
jerome <jerome@geekstudio.co.kr>
2022-06-10 10:55:09 +0900

    :hammer: useMall, useMember Custom Hooks 수정

--- src/hooks/useMember.ts
+++ src/hooks/useMember.ts
@@ -1,39 +1,25 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useQuery } from 'react-query';
-
-import { profile } from 'api/member';
 import { useTypedSelector } from 'state/reducers';
-import { setProfile } from 'state/slices/memberSlice';
-import { isEmptyObject } from 'utils';
-import { useLocation } from 'react-router-dom';
-import { shallowEqual } from 'react-redux';
+import { tokenStorage } from 'utils/storage';
+import { authentication } from 'api/auth';
 
 const useMember = () => {
-    // TODO: 1.멤버정보가 있는지 확인
-    const { token, member } = useTypedSelector(({ token, member }) => ({
-        token,
-        member,
+    const { member } = useTypedSelector(({ member }) => ({
+        member: member.data,
     }));
 
-    // TODO: 토큰이 유효하면 회원정보를 받아온다(expiryTime으로 비교)
-    const { data, isSuccess } = useQuery(
-        'member',
-        async () => await profile.getProfile(token.accessToken),
-        {
-            enabled: isEmptyObject(member),
-            onError: () => {},
-        },
-    );
+    const onLoginClick = () => {};
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (isSuccess) {
-            dispatch(setProfile(data.data));
+    const onLogOutClick = async () => {
+        try {
+            await authentication.deleteAccessToken();
+        } catch (error) {
+            // TODO: ERROR 처리 필요
         }
-    }, [data?.data, isSuccess, dispatch]);
+        tokenStorage.clear();
+        window.location.href = '/';
+    };
 
-    return member;
+    return { member, onLoginClick, onLogOutClick };
 };
 
 export default useMember;
