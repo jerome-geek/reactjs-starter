5ec551d32d8e8399e5fda1d74cb45d8c9d581f58
jerome <jerome@geekstudio.co.kr>
2022-06-17 10:52:32 +0900

    :sparkles: feat: 공지사항 상세페이지 추가

--- src/router/EtcRouter.tsx
+++ src/router/EtcRouter.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import Faq from 'pages/Etc/Faq';
 import NotFound from 'pages/NotFound';
 import Notice from 'pages/Etc/Notice';
+import NoticeDetail from 'pages/Etc/NoticeDetail';
 
 const EtcRouter = () => {
     return (
         <Routes>
             <Route path='notice' element={<Notice />} />
+            <Route path='notice/:articleNo' element={<NoticeDetail />} />
             <Route path='faq' element={<Faq />} />
             <Route path='*' element={<NotFound />} />
         </Routes>
