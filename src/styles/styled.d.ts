b2c203cbe28ffbbbce442cd9ea32aacf2e89c100
jerome <jerome@geekstudio.co.kr>
2022-06-27 15:38:24 +0900

    :sparkles: feat: 프로젝트에서 쓰일 색상 추가

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,19 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        bg1: string;
+        bg2: string;
+        bg3: string;
+        primary: string;
+        secondary: string;
+        line1: string;
+        line2: string;
+        line3: string;
+        text1: string;
+        text2: string;
+        text3: string;
+        text4: string;
+        text5: string;
+    }
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,33 @@
 import { DefaultTheme } from 'styled-components';
 
-export const lightTheme: DefaultTheme = {};
-export const darkTheme: DefaultTheme = {};
+export const lightTheme: DefaultTheme = {
+    bg1: '#191919',
+    bg2: '#F8F8FA',
+    bg3: '#F0F0F3',
+    primary: '#FB1F1F',
+    secondary: '#222943',
+    line1: '#191919',
+    line2: '#DBDBDB',
+    line3: '#EDEDED',
+    text1: '#191919',
+    text2: '#858585',
+    text3: '#A8A8A8',
+    text4: '#FB1F1F',
+    text5: '#518CFF',
+};
+
+export const darkTheme: DefaultTheme = {
+    bg1: '#191919',
+    bg2: '#F8F8FA',
+    bg3: '#F0F0F3',
+    primary: '#FB1F1F',
+    secondary: '#222943',
+    line1: '#191919',
+    line2: '#DBDBDB',
+    line3: '#EDEDED',
+    text1: '#191919',
+    text2: '#858585',
+    text3: '#A8A8A8',
+    text4: '#FB1F1F',
+    text5: '#518CFF',
+};
