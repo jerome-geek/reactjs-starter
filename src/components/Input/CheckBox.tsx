b10478408c1d7f0f6909d2edb5d88cb93f92393d
jerome <jerome@geekstudio.co.kr>
2022-07-15 16:48:21 +0900

    :sparkles: feat: 로그인페이지에서 쓰이는 CheckBox Component 추가

---
+++ src/components/Input/CheckBox.tsx
@@ -0,0 +1,60 @@
+import { forwardRef, InputHTMLAttributes, Ref } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Checked } from 'assets/icons/checked.svg';
+import { ReactComponent as UnChecked } from 'assets/icons/unchecked.svg';
+
+export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+}
+
+const CheckboxContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const CheckBoxLabel = styled.label`
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const HiddenCheckbox = styled.input<CheckboxProps>`
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+`;
+
+const StyledP = styled.p`
+    margin-left: 0.25rem;
+`;
+
+const Checkbox = forwardRef(
+    (
+        { label, id, checked = false, ...props }: CheckboxProps,
+        ref: Ref<HTMLInputElement>,
+    ) => {
+        return (
+            <CheckboxContainer>
+                <CheckBoxLabel htmlFor={props.name}>
+                    <HiddenCheckbox
+                        id={props.name}
+                        type='checkbox'
+                        ref={ref}
+                        {...props}
+                    />
+                    {checked ? <Checked /> : <UnChecked />}
+                    <StyledP>{label}</StyledP>
+                </CheckBoxLabel>
+            </CheckboxContainer>
+        );
+    },
+);
+
+export default Checkbox;
