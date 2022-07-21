5a948309493aa0a103373d9a9143f184b11fd584
jerome <jerome@geekstudio.co.kr>
2022-07-21 16:59:05 +0900

    :hammer: refactor: Header Component 리팩토링

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,91 +1,108 @@
 import { Link, NavLink } from 'react-router-dom';
-import {
-    faCartShopping,
-    faMagnifyingGlass,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
 
+import { useCart, useMember } from 'hooks';
+import media from 'utils/styles/media';
 import PATHS from 'const/paths';
-import { useMember } from 'hooks';
 import categoryNo from 'const/category';
+import { ReactComponent as HeaderLogo } from 'assets/logo/headerLogo.svg';
+import { ReactComponent as MyPageIcon } from 'assets/icons/person.svg';
+import { ReactComponent as SearchIcon } from 'assets/icons/search.svg';
+import { ReactComponent as CartIcon } from 'assets/icons/cart.svg';
+
+const HeaderContainer = styled.header`
+    background-color: #fff;
+    box-shadow: 0 3px 5px #999;
+    padding: 34px 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`;
+
+const LogoContainer = styled.div``;
+
+const NavContainer = styled.nav`
+    ${media.medium} {
+        display: none;
+    }
+
+    & > a {
+        padding: 0 45px;
+    }
+`;
+
+const IconContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > a {
+        position: relative;
+
+        :not(:last-child) {
+            margin-right: 1.5rem;
+        }
+    }
+
+    ${media.medium} {
+        display: none;
+    }
+`;
+
+const CartCount = styled.div`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    background-color: #c00020;
+    border-radius: 50%;
+    width: 12px;
+    height: 12px;
+    font-size: 10px;
+    text-align: center;
+    color: #fff;
+`;
 
 const Header = () => {
     const { member } = useMember();
 
-    const headerStyle = {
-        background: '#fff',
-        boxShadow: '0 3px 5px #999',
-    };
-    const headerContainerStyle = {
-        // display: 'flex',
-        justifyContent: 'space-between',
-        padding: '25px 10px 25px 10px',
-    };
-    const headerLeftStyle = { cssFloat: 'left', display: 'block' };
-    const navContainerStyle = {
-        cssFloat: 'left',
-        marginLeft: '40px',
-        display: 'block',
-    };
-    const navBoxStyle = { marginLeft: '10px' };
-    const headerRightStyle = { cssFloat: 'right', display: 'flex' };
-    const headerRightBoxStyle = { marginRight: '10px' };
+    const { cartInfo } = useCart();
 
     return (
-        <header style={headerStyle}>
-            <div style={headerContainerStyle}>
-                <div style={headerLeftStyle}>
-                    <h1>
-                        <Link to='/'>VOICE CADDIE</Link>
-                    </h1>
-                </div>
-                <nav style={navContainerStyle}>
-                    <NavLink
-                        style={navBoxStyle}
-                        to={`/product/${categoryNo.rangeFinder}`}
-                    >
-                        거리 측정기
-                    </NavLink>
-                    <NavLink
-                        style={navBoxStyle}
-                        to={`/product/${categoryNo.launchMonitor}`}
-                    >
-                        론치 모니터
-                    </NavLink>
-                    <NavLink
-                        style={navBoxStyle}
-                        to={`/product/${categoryNo.accessory}`}
-                    >
-                        액세서리
-                    </NavLink>
-                    <NavLink style={navBoxStyle} to='/'>
-                        고객 서비스
-                    </NavLink>
-                    <NavLink style={navBoxStyle} to='/'>
-                        VSE
-                    </NavLink>
-                </nav>
-                <div style={headerRightStyle}>
-                    {member ? (
-                        <Link style={headerRightBoxStyle} to='/my-page/index'>
-                            {member.memberName}
-                        </Link>
-                    ) : (
-                        <Link style={headerRightBoxStyle} to='/member/login'>
-                            로그인
-                        </Link>
-                    )}
+        <HeaderContainer>
+            <LogoContainer>
+                <Link to='/'>
+                    <HeaderLogo />
+                </Link>
+            </LogoContainer>
 
-                    <Link style={headerRightBoxStyle} to={PATHS.SEARCH}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </Link>
-                    <Link style={headerRightBoxStyle} to={PATHS.CART}>
-                        <FontAwesomeIcon icon={faCartShopping} />
-                    </Link>
-                </div>
-                <div className='after' style={{ clear: 'both' }}></div>
-            </div>
-        </header>
+            <NavContainer>
+                <NavLink to={`/product/${categoryNo.rangeFinder}`}>
+                    거리 측정기
+                </NavLink>
+                <NavLink to={`/product/${categoryNo.launchMonitor}`}>
+                    론치 모니터
+                </NavLink>
+                <NavLink to={`/product/${categoryNo.accessory}`}>
+                    액세서리
+                </NavLink>
+                <NavLink to={PATHS.FAQ}>고객 서비스</NavLink>
+                <NavLink to='/'>VSE</NavLink>
+            </NavContainer>
+
+            <IconContainer>
+                <Link to={member ? PATHS.MY_PAGE : PATHS.LOGIN}>
+                    <MyPageIcon />
+                </Link>
+                <Link to={PATHS.SEARCH}>
+                    <SearchIcon />
+                </Link>
+                <Link to={PATHS.CART}>
+                    <CartIcon />
+                    <CartCount>{cartInfo?.deliveryGroups.length}</CartCount>
+                </Link>
+            </IconContainer>
+        </HeaderContainer>
     );
 };
 
