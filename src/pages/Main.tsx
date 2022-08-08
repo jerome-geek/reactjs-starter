00ae334bb5a2b285fe2b0a10ae73ebe322f960c8
jerome <jerome@geekstudio.co.kr>
2022-08-08 10:29:34 +0900

    :sparkles: feat: BandBanner Component 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+
 import Header from 'components/shared/Header';
+import BandBanner from 'components/shared/BandBanner';
 
 const Main = () => {
+    const [bandBannerVisible, setBandBannerVisible] = useState(true);
+
+    const onBandBannerCloseClick = () => {
+        setBandBannerVisible(false);
+    };
+
     return (
         <>
             <Header />
+            {bandBannerVisible && (
+                <BandBanner
+                    title='신제품 출시 이벤트'
+                    url='/'
+                    onCloseClick={onBandBannerCloseClick}
+                />
+            )}
+
             <div>메인페이지입니다</div>
         </>
     );
