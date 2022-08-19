dd07f7f8c65088c25ba4935e94cc57c7733194ba
jerome <jerome@geekstudio.co.kr>
2022-08-19 12:25:18 +0900

    :sparkles: feat: 메인페이지에 메인 카테고리 배너 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
-import { head } from '@fxts/core';
+import { A11y, Navigation, Pagination } from 'swiper';
+import { SwiperProps } from 'swiper/react';
+import { useWindowSize } from 'usehooks-ts';
+import { sort } from '@fxts/core';
 
 import Header from 'components/shared/Header';
 import BandBanner from 'components/shared/BandBanner';
+import MainSlideBanner from 'components/Main/MainSlideBanner';
+import MainCategoryBanners from 'components/Main/MainCategoryBanners';
+import ETCSection from 'components/Main/ETCSection';
 import { banner } from 'api/display';
 import BANNER from 'const/banner';
 
 const Main = () => {
+    const { width } = useWindowSize();
     const [bandBannerVisible, setBandBannerVisible] = useState(true);
 
     const onBandBannerCloseClick = () => {
@@ -15,28 +22,86 @@ const Main = () => {
     };
 
     const { data: mainBannerData } = useQuery(
-        ['bandBanner'],
-        async () => await banner.getBanners([BANNER.mainBandBanner]),
+        ['mainBanner', BANNER.mainBandBanner],
+        async () =>
+            await banner.getBanners([
+                BANNER.mainBandBanner,
+                BANNER.mainCategoryBanner,
+            ]),
         {
-            select: ({ data }) => head(data),
+            select: ({ data }) => {
+                return {
+                    mainBanner: data[0],
+                    mainCategoryBanner: data[1],
+                };
+            },
         },
+    );
+
+    const settings = useMemo<SwiperProps>(
+        () => ({
+            modules: [Navigation, Pagination, A11y],
+            observer: true,
+            resizeObserver: true,
+            spaceBetween: 50,
+            slidesPerView: 1,
+            navigation: true,
+            pagination: true,
+        }),
+        [],
     );
 
     return (
         <>
             <Header />
+
             {bandBannerVisible && mainBannerData && (
                 <BandBanner
-                    title={mainBannerData.accounts[0].banners[0].name}
+                    title={
+                        mainBannerData.mainBanner.accounts[0].banners[0].name
+                    }
                     url={
-                        mainBannerData.accounts[0].banners[0].videoUrl ??
-                        mainBannerData.accounts[0].banners[0].landingUrl
+                        mainBannerData.mainBanner.accounts[0].banners[0]
+                            .videoUrl ??
+                        mainBannerData.mainBanner.accounts[0].banners[0]
+                            .landingUrl
                     }
                     onCloseClick={onBandBannerCloseClick}
                 />
             )}
 
-            <div>메인페이지입니다</div>
+            {/* 메인 슬라이드 배너 */}
+            {mainBannerData?.mainBanner.accounts[1].banners && (
+                <MainSlideBanner
+                    settings={{
+                        ...settings,
+                        style:
+                            width > 428
+                                ? { height: '828px' }
+                                : { height: '410px' },
+                    }}
+                    banners={mainBannerData.mainBanner.accounts[1]?.banners}
+                />
+            )}
+
+            {/* 카테고리 아이콘 리스트 */}
+            {mainBannerData?.mainCategoryBanner.accounts && (
+                <MainCategoryBanners
+                    banners={sort(
+                        (a, b) => a.displayOrder > b.displayOrder,
+                        mainBannerData?.mainCategoryBanner?.accounts[0].banners,
+                    )}
+                />
+            )}
+
+            {/* ETC */}
+            {mainBannerData?.mainBanner.accounts[4]?.banners && (
+                <ETCSection
+                    iconWidth={mainBannerData?.mainBanner.accounts[4]?.width}
+                    iconHeight={mainBannerData?.mainBanner.accounts[4]?.height}
+                    banners={mainBannerData?.mainBanner.accounts[4]?.banners}
+                />
+            )}
         </>
     );
 };
