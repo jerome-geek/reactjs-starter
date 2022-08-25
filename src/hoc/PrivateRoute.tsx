e155cd267dd033982e2e2ab547f8989ac26f03f7
jerome <jerome@geekstudio.co.kr>
2022-08-25 15:35:04 +0900

    :ambulance: fix: PrivateRoute 오류 수정

--- src/hoc/PrivateRoute.tsx
+++ src/hoc/PrivateRoute.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useLayoutEffect } from 'react';
+import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { useTypedSelector } from 'state/reducers';
+import { useMember } from 'hooks';
 import paths from 'const/paths';
-import { isEmptyObject } from 'utils';
 
 /*
  예)  option: null -> 누구나 출입이 가능한 페이지 (home)
@@ -18,19 +17,17 @@ const PrivateRoute = ({
     option?: Nullable<boolean>;
 }) => {
     const location = useLocation();
-
-    const { member } = useTypedSelector(({ member }) => ({
-        member,
-    }));
-
     const navigate = useNavigate();
 
-    useLayoutEffect(() => {
-        // TODO: 로그인됐을때는?
-        if (isEmptyObject(member)) {
-            option
-                ? navigate(paths.LOGIN, { state: { from: location } })
-                : navigate(paths.MAIN);
+    const { member } = useMember();
+
+    useEffect(() => {
+        if (!member && option) {
+            navigate(paths.LOGIN, { state: { from: location } });
+        }
+
+        if (member && !option) {
+            navigate(paths.MAIN);
         }
     }, [location, navigate, option, member]);
 
