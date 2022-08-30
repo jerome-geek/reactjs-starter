6b9372fc749ce6df74e38ad291d026ec134969ac
jerome <jerome@geekstudio.co.kr>
2022-08-30 15:40:39 +0900

    :sparkles: refactor: Header Component 리팩토링

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,10 +1,13 @@
 import { useMemo, useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useLockedBody, useWindowSize } from 'usehooks-ts';
 
 import MemberPopup from 'components/Member/MemberPopup';
 import SearchLayer from 'components/Search/SearchLayer';
+import SideNavigation from 'components/shared/SideNavigation';
 import { useCart, useMember } from 'hooks';
+import { isDesktop } from 'utils/styles/responsive';
 import media from 'utils/styles/media';
 import PATHS from 'const/paths';
 import CATEGORY from 'const/category';
@@ -12,6 +15,7 @@ import { ReactComponent as HeaderLogo } from 'assets/logo/headerLogo.svg';
 import { ReactComponent as MyPageIcon } from 'assets/icons/person.svg';
 import { ReactComponent as SearchIcon } from 'assets/icons/search.svg';
 import { ReactComponent as CartIcon } from 'assets/icons/cart.svg';
+import { ReactComponent as BarsIcon } from 'assets/icons/bars.svg';
 
 const HeaderContainer = styled.header`
     background-color: #fff;
@@ -23,6 +27,10 @@ const HeaderContainer = styled.header`
     width: 100%;
     position: relative;
     z-index: 1;
+
+    ${media.small} {
+        padding: 24px;
+    }
 `;
 
 const LogoContainer = styled.div``;
@@ -50,10 +58,6 @@ const IconContainer = styled.div`
             margin-right: 1.5rem;
         }
     }
-
-    ${media.medium} {
-        display: none;
-    }
 `;
 
 const MemberName = styled.span`
@@ -80,22 +84,29 @@ const CartCount = styled.div`
 `;
 
 const Header = () => {
-    const [myPageToggle, setMyPageToggle] = useState(false);
-    const [searchToggle, setSearchToggle] = useState(false);
-
-    const navigate = useNavigate();
-
-    const { member, onLoginClick, onLogOutClick } = useMember();
-    const { totalCount } = useCart();
-
-    const onMypageClick = () => setMyPageToggle((prev) => !prev);
-    const onSearchClick = () => setSearchToggle((prev) => !prev);
-
     const headerNavList = useMemo(
         () => [
             {
                 name: '거리 측정기',
                 url: `${PATHS.PRODUCT_LIST}/${CATEGORY.RANGE_FINDER}`,
+                children: [
+                    {
+                        name: '디지털 야디지북',
+                        url: `${PATHS.PRODUCT_LIST}/${CATEGORY.YARDAGE_BOOK}`,
+                    },
+                    {
+                        name: '시계형',
+                        url: `${PATHS.PRODUCT_LIST}/${CATEGORY.CLOCK_TYPE}`,
+                    },
+                    {
+                        name: '레이저형',
+                        url: `${PATHS.PRODUCT_LIST}/${CATEGORY.LASER_TYPE}`,
+                    },
+                    {
+                        name: '음성형',
+                        url: `${PATHS.PRODUCT_LIST}/${CATEGORY.VOICE_TYPE}`,
+                    },
+                ],
             },
             {
                 name: '론치 모니터',
@@ -104,21 +115,74 @@ const Header = () => {
             {
                 name: '액세서리',
                 url: `${PATHS.PRODUCT_LIST}/${CATEGORY.ACCESSORY}`,
-            },
-            {
-                name: '고객 서비스',
-                url: PATHS.FAQ,
+                children: [
+                    {
+                        name: '시계형 충전 케이블',
+                        url: `${PATHS.PRODUCT_LIST}/${CATEGORY.CLOCK_TYPE_CABLE}`,
+                    },
+                    {
+                        name: '보이스캐디 굿즈',
+                        url: `${PATHS.PRODUCT_LIST}/${CATEGORY.VOICECADDIE_GOODS}`,
+                    },
+                    {
+                        name: 'VSE 악세서리',
+                        url: `${PATHS.PRODUCT_LIST}/${CATEGORY.VSE_ACCESSORY}`,
+                    },
+                ],
             },
             {
                 name: 'VSE',
                 url: PATHS.MAIN,
             },
+            {
+                name: '고객 서비스',
+                url: PATHS.FAQ,
+                children: [
+                    { name: '보이스캐디 매니저', url: PATHS.MANAGER },
+                    { name: '매뉴얼', url: PATHS.MANUAL_LIST },
+                    { name: '지원 골프 코스', url: PATHS.GOLF_COURSE_LIST },
+                    { name: '자주하는 질문', url: PATHS.FAQ },
+                    { name: '고객센터' },
+                    { name: '1:1 문의' },
+                    { name: '공지사항', url: PATHS.NOTICE },
+                    { name: '서비스 지원 정책' },
+                ],
+            },
         ],
         [],
     );
 
+    const { width } = useWindowSize();
+
+    const [_, setLocked] = useLockedBody();
+
+    const [myPageToggle, setMyPageToggle] = useState(false);
+    const [searchToggle, setSearchToggle] = useState(false);
+    const [sideNavigationToggle, setSideNavigationToggle] = useState(false);
+
+    const navigate = useNavigate();
+
+    const { member, onLoginClick, onLogOutClick } = useMember();
+    const { totalCount } = useCart();
+
+    const onMypageClick = () => setMyPageToggle((prev) => !prev);
+    const onSearchClick = () => setSearchToggle((prev) => !prev);
+    const onSideNavigationClick = () => {
+        setSideNavigationToggle(true);
+        setLocked(true);
+    };
+    const onCloseButtonClick = () => {
+        setSideNavigationToggle(false);
+        setLocked(false);
+    };
+
     return (
         <>
+            <SideNavigation
+                onCloseButtonClick={onCloseButtonClick}
+                sideNavigationToggle={sideNavigationToggle}
+            />
+
             <HeaderContainer>
                 <LogoContainer>
                     <Link to={PATHS.MAIN}>
@@ -127,9 +191,9 @@ const Header = () => {
                 </LogoContainer>
 
                 <NavContainer>
-                    {headerNavList.map((nav) => (
-                        <NavLink key={nav.url} to={nav.url}>
-                            {nav.name}
+                    {headerNavList.map(({ url, name }) => (
+                        <NavLink key={url} to={url}>
+                            {name}
                         </NavLink>
                     ))}
                 </NavContainer>
@@ -148,13 +212,23 @@ const Header = () => {
                             />
                         )}
                     </div>
-                    <div onClick={onSearchClick}>
-                        <SearchIcon />
-                    </div>
+
+                    {isDesktop(width) && (
+                        <div onClick={onSearchClick}>
+                            <SearchIcon />
+                        </div>
+                    )}
+
                     <div onClick={() => navigate(PATHS.CART)}>
                         <CartIcon />
                         <CartCount>{totalCount}</CartCount>
                     </div>
+
+                    {!isDesktop(width) && (
+                        <div onClick={onSideNavigationClick}>
+                            <BarsIcon />
+                        </div>
+                    )}
                 </IconContainer>
             </HeaderContainer>
             {searchToggle && <SearchLayer />}
