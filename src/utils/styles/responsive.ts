9a152ea551e996a2e5205243ab7035c08700fdc7
jerome <jerome@geekstudio.co.kr>
2022-08-30 17:57:02 +0900

    :ambulance: fix: 오타수정

--- src/utils/styles/responsive.ts
+++ src/utils/styles/responsive.ts
@@ -1,8 +1,8 @@
 import BREAKPOINTS from 'const/breakpoints';
 
 const isMobile = (width: number) => width < BREAKPOINTS.MEDIUM;
-const isTable = (width: number) =>
+const isTablet = (width: number) =>
     width >= BREAKPOINTS.MEDIUM && width < BREAKPOINTS.LARGE;
 const isDesktop = (width: number) => width >= BREAKPOINTS.LARGE;
 
-export { isMobile, isTable, isDesktop };
+export { isMobile, isTablet, isDesktop };
