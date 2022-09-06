15b52d605d8850cdcab78cdd4fc4f75a7be4230d
jerome <jerome@geekstudio.co.kr>
2022-09-06 17:26:09 +0900

    :hammer: refactor: 반응형 관련 사이즈 수정

--- src/utils/styles/responsive.ts
+++ src/utils/styles/responsive.ts
@@ -2,7 +2,7 @@ import BREAKPOINTS from 'const/breakpoints';
 
 const isMobile = (width: number) => width < BREAKPOINTS.MEDIUM;
 const isTablet = (width: number) =>
-    width >= BREAKPOINTS.MEDIUM && width < BREAKPOINTS.LARGE;
-const isDesktop = (width: number) => width >= BREAKPOINTS.LARGE;
+    width >= BREAKPOINTS.MEDIUM && width < BREAKPOINTS.XLARGE;
+const isDesktop = (width: number) => width >= BREAKPOINTS.XLARGE;
 
 export { isMobile, isTablet, isDesktop };
