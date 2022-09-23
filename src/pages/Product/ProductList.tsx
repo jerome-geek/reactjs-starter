fdba97e121629531680c668286c2118ea9e3ce2c
jerome <jerome@geekstudio.co.kr>
2022-09-23 15:18:20 +0900

    :hammer: refactor: 상품 리스트 페이지 리팩토링

--- src/pages/Product/ProductList.tsx
+++ src/pages/Product/ProductList.tsx
@@ -1,214 +1,339 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import { useEffect, useState, useMemo } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
-import { head } from '@fxts/core';
+import styled from 'styled-components';
+import { head, map, pipe, toArray, filter, concat } from '@fxts/core';
 
-import { category } from 'api/display';
-import { product } from 'api/product';
 import Header from 'components/shared/Header';
-import { filter } from 'const/filter';
-import { PRODUCT_BY, ORDER_DIRECTION } from 'models';
 import SEOHelmet from 'components/shared/SEOHelmet';
+import ProductSort from 'components/Product/ProductSort';
+import ProductCard from 'components/Search/ProductCard';
 import Loader from 'components/shared/Loader';
-import currency from 'currency.js';
+import { product } from 'api/product';
+import media from 'utils/styles/media';
+import PATHS from 'const/paths';
+import { useMall } from 'hooks';
+import { PRODUCT_BY, ORDER_DIRECTION } from 'models';
+import { MultiLevelCategory } from 'models/display';
+import { ProductItem } from 'models/product';
+
+interface ProductCategory {
+    categoryNo: string;
+    title: string;
+    isActive: boolean;
+}
 
 const ProductListContainer = styled.div`
-    padding: 0 20px;
+    max-width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
+
+    ${media.xxlarge} {
+        width: calc(100% - 2rem);
+    }
+
+    ${media.small} {
+        /* width: 380px; */
+        margin-top: 24px;
+    }
 `;
 
-const ProductListTop = styled.div``;
-
-const ProductListTitle = styled.h2``;
-
-const ProductListCategoryBox = styled.div`
+const ProductListTopContainer = styled.div`
+    margin-top: 150px;
     display: flex;
+    justify-content: space-between;
+    align-items: end;
+    margin-bottom: 50px;
 `;
 
-const ProductListCategory = styled.div``;
+const ProductListBannerContainer = styled.div`
+    max-width: 100%;
+    margin-bottom: 40px;
+    text-align: center;
 
-const ProductListFilterBox = styled.select``;
+    & img {
+        max-width: 100%;
+    }
+`;
 
-const ProductListFilter = styled.option``;
-
-const ProductContainer = styled.div`
+const ProductListDownContainer = styled.section`
+    margin-top: 40px;
     display: flex;
     flex-wrap: wrap;
-    margin: 0 auto;
+    height: auto;
+    overflow: auto;
 `;
 
-const ProductBox = styled.div`
-    width: 25%;
-    padding: 15px;
-    > img {
-        display: block;
-        width: 100%;
+const ProductListTitle = styled.h2`
+    font-size: 1.5rem;
+    letter-spacing: -1.2px;
+    line-height: 36px;
+    font-weight: bold;
+    color: #191919;
+
+    ${media.medium} {
+        font-size: 20px;
+        letter-spacing: -1px;
+    }
+`;
+
+const ProductCategoryListContainer = styled.div``;
+
+const ProductCategoryList = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const ProductCategoryListItem = styled.li<{ isActive?: boolean }>`
+    flex: 1;
+    margin: 0 1%;
+    text-align: center;
+    white-space: nowrap;
+
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-size: 16px;
+    color: ${({ isActive }) => (isActive ? '#fff' : '#8F8F8F')};
+    background-color: ${({ isActive }) => (isActive ? '#222943' : '#fff')};
+    border: ${({ isActive }) => (isActive ? '' : '1px solid #DBDBDB')};
+
+    &:first-child {
+        margin-left: 0;
+    }
+    &:last-child {
+        margin-right: 0;
     }
 `;
 
 const ProductList = () => {
-    const { categoryNo } = useParams<{ categoryNo: string }>();
-    const [orderBy, setOrderBy] = useState(PRODUCT_BY.RECENT_PRODUCT);
-    const [direction, setDirection] = useState(ORDER_DIRECTION.DESC);
-    const [currentCategoryNo, setCurrentCategoryNo] =
-        useState<string | undefined>(categoryNo);
+    const { categoryNo } = useParams() as { categoryNo: string };
+
     const navigate = useNavigate();
 
-    useEffect(() => {
-        setCurrentCategoryNo(categoryNo);
-    }, [categoryNo]);
+    const [mall] = useMall();
 
-    const { data: productCategoryList, isFetching: categoryFetching } =
-        useQuery(
-            ['productCategoryList', { categoryNo }],
-            async () => await category.getCategory(categoryNo!),
-            {
-                select: ({ data }) => {
-                    return data?.multiLevelCategories;
-                },
-                refetchOnWindowFocus: false,
-            },
+    const categoryInfo = useMemo<any>(
+        () =>
+            pipe(
+                mall.categories.multiLevelCategories,
+                filter((a: any) => a.categoryNo.toString() === categoryNo),
+                toArray,
+                head,
+            ),
+        [categoryNo, mall.categories.multiLevelCategories],
+    );
+
+    const [productCategory, setProductCategory] = useState<ProductCategory[]>([
+        {
+            categoryNo,
+            title: '전체보기',
+            isActive: true,
+        },
+    ]);
+
+    useEffect(() => {
+        setProductCategory((prev) =>
+            pipe<
+                MultiLevelCategory[],
+                Iterable<ProductCategory>,
+                Iterable<ProductCategory>,
+                ProductCategory[]
+            >(
+                categoryInfo.children,
+                map((a) => ({
+                    categoryNo: a.categoryNo.toString(),
+                    title: a.label,
+                    isActive: false,
+                })),
+                concat(prev),
+                toArray,
+            ),
         );
 
-    const { data: productItems, isFetching: productFetching } = useQuery(
-        ['productList', { currentCategoryNo, orderBy, direction }],
-        async () =>
-            await product.searchProducts({
-                categoryNos: currentCategoryNo,
-                hasOptionValues: true,
-                order: { by: orderBy, direction: direction },
-            }),
+        return () => {
+            setProductCategory([
+                {
+                    categoryNo: categoryNo,
+                    title: '전체보기',
+                    isActive: true,
+                },
+            ]);
+        };
+    }, [categoryNo, categoryInfo.children]);
+
+    const [productSort, setProductSort] = useState([
+        {
+            id: 'recent',
+            title: '최신순',
+            by: PRODUCT_BY.RECENT_PRODUCT,
+            direction: ORDER_DIRECTION.DESC,
+            isActive: true,
+        },
+        {
+            id: 'manyOrders',
+            title: '주문많은순',
+            by: PRODUCT_BY.POPULAR,
+            direction: ORDER_DIRECTION.DESC,
+            isActive: false,
+        },
+        {
+            id: 'lowPrice',
+            title: '가격낮은순',
+            by: PRODUCT_BY.DISCOUNTED_PRICE,
+            direction: ORDER_DIRECTION.ASC,
+            isActive: false,
+        },
+        {
+            id: 'highPrice',
+            title: '가격 높은순',
+            by: PRODUCT_BY.DISCOUNTED_PRICE,
+            direction: ORDER_DIRECTION.DESC,
+            isActive: false,
+        },
+    ]);
+
+    const onProductSortClick = (id: string) => {
+        setProductSort((prev) =>
+            pipe(
+                prev,
+                map((a) => ({ ...a, isActive: a.id === id })),
+                toArray,
+            ),
+        );
+
+        setSearchParams((prev) => {
+            const selectedproductSort = pipe(
+                productSort,
+                filter((a) => a.id === id),
+                toArray,
+                head,
+            );
+
+            return Object.assign({}, prev, {
+                order: {
+                    by: selectedproductSort?.by,
+                    direction: selectedproductSort?.direction,
+                },
+            });
+        });
+    };
+
+    const [searchParams, setSearchParams] = useState({
+        categoryNos: categoryNo,
+        pageNumber: 1,
+        pageSize: 10,
+        order: {
+            by: PRODUCT_BY.RECENT_PRODUCT,
+            direction: ORDER_DIRECTION.DESC,
+        },
+        hasOptionValues: true,
+    });
+
+    useEffect(() => {
+        setSearchParams((prev) => {
+            return { ...prev, categoryNos: categoryNo };
+        });
+    }, [categoryNo]);
+
+    const onProductCategoryClick = (categoryNo: string) => {
+        setProductCategory((prev) =>
+            pipe(
+                prev,
+                map((a) => ({
+                    ...a,
+                    isActive: a.categoryNo === categoryNo,
+                })),
+                toArray,
+            ),
+        );
+        setSearchParams((prev) => ({ ...prev, categoryNos: categoryNo }));
+    };
+
+    const { data: productList, isFetching: isProductListFetching } = useQuery(
+        ['productList', searchParams],
+        async () => await product.searchProducts(searchParams),
         {
             select: (res) => {
                 return res.data.items;
             },
-            refetchOnWindowFocus: false,
         },
     );
 
-    const filterChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const [orderBy, orderDirection] = e.target.value.split(',');
-        setOrderBy(orderBy as PRODUCT_BY);
-        setDirection(orderDirection as ORDER_DIRECTION);
-    };
-
     return (
         <>
-            <SEOHelmet
-                data={{
-                    title:
-                        productCategoryList && head(productCategoryList)?.label,
-                }}
-            />
+            <SEOHelmet data={{ title: categoryInfo?.label }} />
+
             <Header />
-            {categoryFetching ? (
-                <Loader />
-            ) : (
-                productCategoryList && (
-                    <ProductListContainer>
-                        <ProductListTop>
-                            <ProductListTitle>
-                                {head(productCategoryList)?.label}
-                            </ProductListTitle>
-                            <ProductListCategoryBox>
-                                {head(productCategoryList)?.children?.length! >
-                                    0 && (
-                                    <ProductListCategory
+
+            <ProductListContainer>
+                <ProductListTopContainer>
+                    <ProductListTitle>{categoryInfo?.label}</ProductListTitle>
+
+                    <ProductSort
+                        productSort={productSort}
+                        onProductSortClick={onProductSortClick}
+                    />
+                </ProductListTopContainer>
+            </ProductListContainer>
+
+            {categoryInfo?.content && (
+                <ProductListBannerContainer
+                    dangerouslySetInnerHTML={{ __html: categoryInfo?.content }}
+                />
+            )}
+
+            <ProductListContainer>
+                <ProductCategoryListContainer>
+                    <ProductCategoryList>
+                        {productCategory.length > 1 &&
+                            productCategory.map(
+                                ({ categoryNo, isActive, title }) => (
+                                    <ProductCategoryListItem
                                         key={categoryNo}
+                                        isActive={isActive}
                                         onClick={() =>
-                                            setCurrentCategoryNo(categoryNo)
+                                            onProductCategoryClick(categoryNo)
                                         }
                                     >
-                                        전체보기
-                                    </ProductListCategory>
-                                )}
-                                {head(productCategoryList)?.children?.map(
-                                    ({ categoryNo, label }) => {
-                                        return (
-                                            <ProductListCategory
-                                                key={categoryNo}
-                                                onClick={() =>
-                                                    setCurrentCategoryNo(
-                                                        categoryNo.toString(),
-                                                    )
-                                                }
-                                            >
-                                                {label}
-                                            </ProductListCategory>
-                                        );
-                                    },
-                                )}
-                            </ProductListCategoryBox>
-                            <ProductListFilterBox
-                                onChange={filterChangeHandler}
-                            >
-                                {filter.map(({ title, orderBy, direction }) => {
-                                    return (
-                                        <ProductListFilter
-                                            key={title}
-                                            value={[orderBy, direction]}
-                                        >
-                                            {title}
-                                        </ProductListFilter>
-                                    );
-                                })}
-                            </ProductListFilterBox>
-                        </ProductListTop>
-                        {productFetching ? (
-                            <Loader />
-                        ) : (
-                            <ProductContainer>
-                                {productItems &&
-                                    productItems.map(
-                                        ({
-                                            productNo,
-                                            listImageUrls,
-                                            productName,
-                                            promotionText,
-                                            salePrice,
-                                            immediateDiscountAmt,
-                                        }: any) => {
-                                            return (
-                                                <ProductBox
-                                                    key={productNo}
-                                                    onClick={() => {
-                                                        navigate(
-                                                            `/product/detail/${productNo}`,
-                                                        );
-                                                    }}
-                                                >
-                                                    <img
-                                                        src={
-                                                            head(
-                                                                listImageUrls,
-                                                            ) as
-                                                                | string
-                                                                | undefined
-                                                        }
-                                                        alt={productName}
-                                                    />
-                                                    <p>{productName}</p>
-                                                    <p>{promotionText}</p>
-                                                    <p>
-                                                        {currency(
-                                                            salePrice -
-                                                                immediateDiscountAmt,
-                                                            {
-                                                                symbol: '',
-                                                                precision: 0,
-                                                            },
-                                                        ).format()}
-                                                        <span>원</span>
-                                                    </p>
-                                                </ProductBox>
-                                            );
-                                        },
-                                    )}
-                            </ProductContainer>
-                        )}
-                    </ProductListContainer>
-                )
-            )}
+                                        {title}
+                                    </ProductCategoryListItem>
+                                ),
+                            )}
+                    </ProductCategoryList>
+                </ProductCategoryListContainer>
+
+                <ProductListDownContainer>
+                    {isProductListFetching ? (
+                        <Loader type='oval' />
+                    ) : (
+                        productList.map(
+                            ({
+                                productNo,
+                                productName,
+                                salePrice,
+                                listImageUrls,
+                                promotionText,
+                                immediateDiscountAmt,
+                            }: ProductItem) => (
+                                <ProductCard
+                                    onClick={() =>
+                                        navigate(
+                                            `${PATHS.PRODUCT_DETAIL}/${productNo}`,
+                                        )
+                                    }
+                                    key={productNo}
+                                    imgUrl={head<string[]>(listImageUrls) || ''}
+                                    productName={productName}
+                                    promotionText={promotionText || ''}
+                                    salePrice={salePrice}
+                                    immediateDiscountAmt={immediateDiscountAmt}
+                                />
+                            ),
+                        )
+                    )}
+                </ProductListDownContainer>
+            </ProductListContainer>
         </>
     );
 };
