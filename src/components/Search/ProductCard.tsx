725eaccd90d947b3ac69fe2293fab77724ecbadf
jerome <jerome@geekstudio.co.kr>
2022-09-23 15:14:24 +0900

    :sparkles: feat: ProductCard Component 추가

---
+++ src/components/Search/ProductCard.tsx
@@ -0,0 +1,159 @@
+import { FC, HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import { KRW } from 'utils/currency';
+import media from 'utils/styles/media';
+import { ellipsis } from 'utils/styles/mixin';
+
+interface ProductCardProps extends HTMLAttributes<HTMLDivElement> {
+    imgUrl: string;
+    productName: string;
+    promotionText: string;
+    salePrice: number;
+    immediateDiscountAmt: number;
+}
+
+const ProductCardContainer = styled.div`
+    width: 31%;
+    margin-bottom: 80px;
+
+    &:not(:nth-child(3n)) {
+        margin-right: 40px;
+    }
+
+    ${media.medium} {
+        width: 47%;
+        margin-bottom: 20px;
+
+        &:not(:nth-child(3n)) {
+            margin-right: 8px;
+        }
+    }
+`;
+
+const ProductCardImageContainer = styled.div`
+    background-color: #f8f8fa;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ProductCardImage = styled.div`
+    & > img {
+        width: 100%;
+        max-width: 100%;
+        height: auto;
+    }
+`;
+
+const ProductCardContentsContainer = styled.div`
+    padding: 20px 10px;
+
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+
+    ${media.medium} {
+        flex-direction: column;
+    }
+`;
+
+const ProductInfoContainer = styled.div`
+    ${media.medium} {
+        margin-bottom: 10px;
+    }
+`;
+
+const ProductName = styled.p`
+    font-size: 16px;
+    font-weight: medium;
+    text-align: left;
+    color: #191919;
+    /* margin-bottom: 4px; */
+    line-height: 22px;
+`;
+
+const ProductDescription = styled.p`
+    font-size: 16px;
+    font-weight: normal;
+    text-align: left;
+    color: #858585;
+    letter-spacing: -0.6px;
+    line-height: 22px;
+
+    ${ellipsis(1)}
+
+    ${media.medium} {
+        font-size: 12px;
+        letter-spacing: -0.48px;
+    }
+`;
+
+const ProductPriceContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    ${media.medium} {
+        flex-direction: row;
+    }
+`;
+
+const SalePrice = styled.p`
+    letter-spacing: 0px;
+    color: #191919;
+    text-align: right;
+    font-size: 16px;
+    line-height: 22px;
+
+    ${media.medium} {
+        font-size: 14px;
+        margin-right: 10px;
+    }
+`;
+
+const ProductPrice = styled.p`
+    color: #a8a8a8;
+    text-align: right;
+    text-decoration: line-through;
+    font-size: 16px;
+    line-height: 22px;
+
+    ${media.medium} {
+        font-size: 12px;
+    }
+`;
+
+const ProductCard: FC<ProductCardProps> = ({
+    imgUrl,
+    productName,
+    promotionText,
+    salePrice,
+    immediateDiscountAmt = 0,
+    ...props
+}) => {
+    return (
+        <ProductCardContainer {...props}>
+            <ProductCardImageContainer>
+                <ProductCardImage>
+                    <img src={imgUrl} alt={productName} />
+                </ProductCardImage>
+            </ProductCardImageContainer>
+
+            <ProductCardContentsContainer>
+                <ProductInfoContainer>
+                    <ProductName>{productName}</ProductName>
+                    <ProductDescription>{promotionText}</ProductDescription>
+                </ProductInfoContainer>
+
+                <ProductPriceContainer>
+                    <SalePrice>
+                        {KRW(salePrice - immediateDiscountAmt).format()}
+                    </SalePrice>
+
+                    <ProductPrice>{KRW(salePrice).format()}</ProductPrice>
+                </ProductPriceContainer>
+            </ProductCardContentsContainer>
+        </ProductCardContainer>
+    );
+};
+
+export default ProductCard;
