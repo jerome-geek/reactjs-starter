be4fe0e50df43a7bad9c01b111454bad72f0da82
jerome <jerome@geekstudio.co.kr>
2022-10-06 18:48:56 +0900

    :hammer: refactor: CommonPayment Component 수정

--- src/components/OrderSheet/CommonPayment.tsx
+++ src/components/OrderSheet/CommonPayment.tsx
@@ -1,146 +1,151 @@
-import { useState } from 'react';
+import { FC } from 'react';
 import { UseFormSetValue } from 'react-hook-form';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useWindowSize } from 'usehooks-ts';
 import { useTranslation } from 'react-i18next';
 
-import { PAY_TYPE } from 'models';
-import { PaymentReserve } from 'models/order';
+import PrimaryButton from 'components/Button/PrimaryButton';
 import { isMobile } from 'utils/styles/responsive';
 import media from 'utils/styles/media';
+import { PAY_TYPE } from 'models';
+import { PaymentReserve } from 'models/order';
+import PAY_TYPES from 'const/payTypes';
 
-const CommonPaymentContainer = styled.div`
+interface CommomPaymentProps {
+    setValue: UseFormSetValue<PaymentReserve>;
+    payType: PAY_TYPE;
+    availablePayTypes: {
+        payType: string;
+        pgTypes: string[];
+    }[];
+}
+
+const CommonPaymentContainer = styled.div``;
+
+const PaymentTitle = styled.h3`
+    font-size: 24px;
+    letter-spacing: -1.2px;
+    line-height: 36px;
+    font-weight: bold;
+    color: #191919;
+    margin-bottom: 20px;
+
+    ${media.medium} {
+        font-size: 18px;
+        letter-spacing: -0.72px;
+    }
+`;
+
+const PaymentContainer = styled.div`
     border-top: 2px solid #222943;
     border-bottom: 2px solid #222943;
 `;
 
-const SheetInputWrapper = styled.div`
+const PaymentContentContainer = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: space-between;
     border-bottom: 1px solid #dbdbdb;
     text-align: left;
     min-height: 104px;
+
     &:last-child {
         border-bottom: none;
     }
 `;
 
-const SheetInputTitleBox = styled.div`
-    width: 200px;
-    padding: 44px 0 40px 41px;
-    display: flex;
-    flex-direction: column;
+const DefaultPaymentTitle = styled.p`
+    font-size: 1rem;
+    letter-spacing: 0;
+    line-height: 24px;
+    color: #191919;
+    padding-right: 120px;
 `;
 
-const SheetInputBox = styled.div`
-    width: 550px;
+const PaymentList = styled.div`
     padding-top: 30px;
     padding-bottom: 20px;
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     flex-wrap: wrap;
     align-items: center;
     color: ${(props) => props.theme.text1};
-    > #defaultAddress {
-        display: none;
-    }
-    > label {
-        display: flex;
-        align-items: center;
-        > p {
-            color: #8f8f8f;
-            margin-left: 10px;
-        }
-    }
+
     ${media.medium} {
+        justify-content: space-between;
         width: 100%;
     }
 `;
 
-const SheetButton = styled.div<{ width: string; isClicked: boolean }>`
-    width: ${(props) => props.width};
-    height: 44px;
-    line-height: 44px;
-    text-align: center;
-    color: ${(props) => (props.isClicked ? '#fff' : props.theme.text3)};
-    background: ${(props) =>
-        props.isClicked ? props.theme.secondary : props.theme.bg2};
+const PaymentListItem = styled(PrimaryButton)<{ isClicked: boolean }>`
+    width: 31%;
+    padding: 10px 26px;
+    box-sizing: border-box;
+    margin-right: 10px;
     margin-bottom: 10px;
-    cursor: pointer;
+
+    ${(props) =>
+        !props.isClicked &&
+        css`
+            color: ${(props) => props.theme.text3};
+            background-color: ${(props) => props.theme.bg2};
+            border: 1px solid ${(props) => props.theme.bg2};
+        `};
+
     ${media.medium} {
-        width: 48.9%;
-        height: 54px;
-        text-align: center;
-        line-height: 54px;
-        font-size: 1rem;
-    }
-    ${media.small} {
-        font-size: 1.6rem;
+        margin-bottom: 8px;
+        flex: 0 0 48%;
+
+        &:nth-child(2n + 1) {
+            margin-right: 8px;
+        }
+        &:nth-child(2n) {
+            margin-right: 0;
+        }
     }
 `;
 
-const CommonPayment = ({
+const CommonPayment: FC<CommomPaymentProps> = ({
     setValue,
-}: {
-    setValue: UseFormSetValue<PaymentReserve>;
+    payType,
+    availablePayTypes,
 }) => {
-    const [paymentMethod, setPaymentMethod] = useState<PAY_TYPE>(
-        PAY_TYPE.CREDIT_CARD,
-    );
+    const { t: orderSheet } = useTranslation('orderSheet');
 
     const { width } = useWindowSize();
 
-    const changePaymentMethodHandler = (payType: PAY_TYPE) => () => {
-        setPaymentMethod(payType);
-        setValue('payType', payType);
-    };
-
-    const { t: sheet } = useTranslation('orderSheet');
-
     return (
         <CommonPaymentContainer>
-            <SheetInputWrapper>
-                {!isMobile(width) && (
-                    <SheetInputTitleBox>일반 결제</SheetInputTitleBox>
-                )}
-                <SheetInputBox>
-                    <SheetButton
-                        isClicked={paymentMethod === PAY_TYPE.CREDIT_CARD}
-                        width='20%'
-                        onClick={changePaymentMethodHandler(
-                            PAY_TYPE.CREDIT_CARD,
-                        )}
-                    >
-                        신용카드
-                    </SheetButton>
-                    <SheetButton
-                        isClicked={
-                            paymentMethod === PAY_TYPE.REALTIME_ACCOUNT_TRANSFER
-                        }
-                        width='29.09%'
-                        onClick={changePaymentMethodHandler(
-                            PAY_TYPE.REALTIME_ACCOUNT_TRANSFER,
-                        )}
-                    >
-                        실시간 계좌 이체
-                    </SheetButton>
-                    <SheetButton
-                        isClicked={paymentMethod === PAY_TYPE.VIRTUAL_ACCOUNT}
-                        width='20%'
-                        onClick={changePaymentMethodHandler(
-                            PAY_TYPE.VIRTUAL_ACCOUNT,
-                        )}
-                    >
-                        가상계좌
-                    </SheetButton>
-                    <SheetButton
-                        isClicked={paymentMethod === PAY_TYPE.KPAY}
-                        width='20%'
-                        onClick={changePaymentMethodHandler(PAY_TYPE.KPAY)}
-                    >
-                        Kpay
-                    </SheetButton>
-                </SheetInputBox>
-            </SheetInputWrapper>
+            <PaymentTitle>{orderSheet('paymentMethod.title')}</PaymentTitle>
+
+            <PaymentContainer>
+                <PaymentContentContainer>
+                    {!isMobile(width) && (
+                        <DefaultPaymentTitle>일반 결제</DefaultPaymentTitle>
+                    )}
+                    <PaymentList>
+                        {availablePayTypes.map((availablePayType) => (
+                            <PaymentListItem
+                                key={availablePayType.payType}
+                                isClicked={payType === availablePayType.payType}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setValue(
+                                        'payType',
+                                        availablePayType.payType as PAY_TYPE,
+                                    );
+                                }}
+                            >
+                                {
+                                    PAY_TYPES[
+                                        availablePayType.payType as PAY_TYPE
+                                    ]
+                                }
+                            </PaymentListItem>
+                        ))}
+                    </PaymentList>
+                </PaymentContentContainer>
+            </PaymentContainer>
         </CommonPaymentContainer>
     );
 };
