630ab8b446e911a4b68d7c02eabf1105ea6db39d
jerome <jerome@geekstudio.co.kr>
2022-10-17 11:54:01 +0900

    :sparkles: feat: AccumulationInfo Component 추가

---
+++ src/components/Product/AccumulationInfo.tsx
@@ -0,0 +1,164 @@
+import React, { FC, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+import { useMall } from 'hooks';
+import { KRW } from 'utils/currency';
+
+interface AccumulationInfoProps {
+    accumulationAmtWhenBuyConfirm?: number;
+}
+
+const AccumulationInfoContainer = styled.div`
+    display: flex;
+    align-items: flex-start;
+    padding-bottom: 26px;
+    border-bottom: 2px solid #ededed;
+    margin-bottom: 26px;
+`;
+
+const AccumulationInfoTitle = styled.p`
+    font-size: 16px;
+    letter-spacing: 0;
+    color: #191919;
+    margin-right: 20px;
+    font-weight: normal;
+`;
+
+const AccumulationInfoContent = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const AccumulationBenefits = styled.span`
+    > b {
+        color: ${(props) => props.theme.main};
+    }
+
+    &:not(:last-of-type) {
+        margin-bottom: 8px;
+    }
+`;
+
+const AccumulationInfo: FC<AccumulationInfoProps> = ({
+    accumulationAmtWhenBuyConfirm = 0,
+}) => {
+    const { t: productDetail } = useTranslation('productDetail');
+
+    const { mallInfo } = useMall();
+
+    const accumulationAmtWhenBuyConfirmInfo = useMemo(
+        () =>
+            accumulationAmtWhenBuyConfirm === 0
+                ? ''
+                : `구매확정시 ${KRW(accumulationAmtWhenBuyConfirm, {
+                      symbol: mallInfo.accumulationConfig.accumulationUnit,
+                      precision: 0,
+                      pattern: `<b># !</b>`,
+                  }).format()} 즉시 지급`,
+        [
+            accumulationAmtWhenBuyConfirm,
+            mallInfo.accumulationConfig.accumulationUnit,
+        ],
+    );
+
+    const reviewsAccumulation = useMemo(
+        () =>
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.reviewsAccumulation === 0
+                ? ''
+                : `
+    리뷰작성시 ${KRW(
+        mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+            ?.reviewsAccumulation,
+        {
+            symbol: mallInfo?.accumulationConfig?.accumulationUnit,
+            precision: 0,
+            pattern: `<b># !</b>`,
+        },
+    ).format()} 즉시 지급`,
+        [
+            mallInfo?.accumulationConfig?.accumulationUnit,
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.reviewsAccumulation,
+        ],
+    );
+
+    const photoReviewsAccumulation = useMemo(
+        () =>
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.photoReviewsAccumulation === 0
+                ? ''
+                : `
+        포토리뷰작성시 ${KRW(
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.photoReviewsAccumulation,
+            {
+                symbol: mallInfo?.accumulationConfig?.accumulationUnit,
+                precision: 0,
+                pattern: `<b># !</b>`,
+            },
+        ).format()} 즉시 지급`,
+        [
+            mallInfo?.accumulationConfig?.accumulationUnit,
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.photoReviewsAccumulation,
+        ],
+    );
+
+    const isAccumulationInfoVisible = useMemo(
+        () =>
+            accumulationAmtWhenBuyConfirm !== 0 ||
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.reviewsAccumulation !== 0 ||
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.photoReviewsAccumulation !== 0,
+        [
+            accumulationAmtWhenBuyConfirm,
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.reviewsAccumulation,
+            mallInfo?.accumulationConfig?.reviewsAccumulationDetail
+                ?.photoReviewsAccumulation,
+        ],
+    );
+
+    return (
+        <>
+            {isAccumulationInfoVisible && (
+                <AccumulationInfoContainer>
+                    <AccumulationInfoTitle>
+                        {productDetail('accumulateBenefits')}
+                    </AccumulationInfoTitle>
+
+                    <AccumulationInfoContent>
+                        {accumulationAmtWhenBuyConfirmInfo && (
+                            <AccumulationBenefits
+                                dangerouslySetInnerHTML={{
+                                    __html: accumulationAmtWhenBuyConfirmInfo,
+                                }}
+                            />
+                        )}
+
+                        {reviewsAccumulation && (
+                            <AccumulationBenefits
+                                dangerouslySetInnerHTML={{
+                                    __html: reviewsAccumulation,
+                                }}
+                            />
+                        )}
+
+                        {photoReviewsAccumulation && (
+                            <AccumulationBenefits
+                                dangerouslySetInnerHTML={{
+                                    __html: photoReviewsAccumulation,
+                                }}
+                            />
+                        )}
+                    </AccumulationInfoContent>
+                </AccumulationInfoContainer>
+            )}
+        </>
+    );
+};
+
+export default AccumulationInfo;
