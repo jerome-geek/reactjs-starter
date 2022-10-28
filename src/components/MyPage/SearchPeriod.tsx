4752cfbf37ffbe79dd6d0acbdd3a6ea5e8b607ac
jerome <jerome@geekstudio.co.kr>
2022-10-28 11:30:00 +0900

    :sparkles: feat: 마이페이지 > 주문리스트에 날짜검색 기능 추가

--- src/components/MyPage/SearchPeriod.tsx
+++ src/components/MyPage/SearchPeriod.tsx
@@ -1,14 +1,19 @@
 import { FC, HTMLAttributes, useState, Dispatch, SetStateAction } from 'react';
 import styled from 'styled-components';
 import dayjs from 'dayjs';
-import { map, pipe, toArray, filter, head } from '@fxts/core';
+import { map, pipe, toArray } from '@fxts/core';
+import ReactDatePicker from 'react-datepicker';
+import { ko } from 'date-fns/esm/locale';
 
 import InputWithCalendarIcon from 'components/Input/InputWithCalendarIcon';
 import PrimaryButton from 'components/Button/PrimaryButton';
+import { flex } from 'utils/styles/mixin';
+import 'react-datepicker/dist/react-datepicker.css';
 
 interface SearchPeriodProps extends HTMLAttributes<HTMLDivElement> {
     startYmd: string;
     endYmd: string;
+    searchCondition: Paging & SearchDate;
     setSearchCondition: Dispatch<SetStateAction<Paging & SearchDate>>;
 }
 
@@ -18,9 +23,10 @@ interface PeriodTab {
     isSelected: boolean;
 }
 
-const SearchPeriodWrapper = styled.div`
+const SearchPeriodContainer = styled.div`
     display: flex;
     align-items: center;
+    justify-content: space-evenly;
     margin-bottom: 40px;
 `;
 
@@ -35,10 +41,21 @@ const PeriodButton = styled.button<{ selected: boolean }>`
     margin-right: 8px;
 `;
 
-const PeriodWrapper = styled.div`
+const PeriodTabContainer = styled.div`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
+`;
+
+const PeriodContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+const DatePickerContainer = styled.div`
+    ${flex}
+    padding: 0 20px;
 `;
 
 const SearchButton = styled(PrimaryButton)`
@@ -48,18 +65,21 @@ const SearchButton = styled(PrimaryButton)`
 `;
 
 const SearchPeriod: FC<SearchPeriodProps> = ({
-    startYmd,
-    endYmd,
+    searchCondition,
     setSearchCondition,
 }) => {
     const [periodTab, setPeriodTab] = useState<PeriodTab[]>([
         { period: 0, name: '오늘', isSelected: false },
-        { period: 7, name: '7일', isSelected: true },
+        { period: 7, name: '7일', isSelected: false },
         { period: 15, name: '15일', isSelected: false },
         { period: 30, name: '1개월', isSelected: false },
-        { period: 90, name: '3개월', isSelected: false },
+        { period: 90, name: '3개월', isSelected: true },
         { period: 365, name: '1년', isSelected: false },
     ]);
+    const [startYmd, setStartYmd] = useState(
+        new Date(searchCondition.startYmd),
+    );
+    const [endYmd, setEndYmd] = useState(new Date(searchCondition.endYmd));
 
     const onPeriodTabClick = (period: number) => {
         setPeriodTab((prev) =>
@@ -72,28 +92,25 @@ const SearchPeriod: FC<SearchPeriodProps> = ({
                 toArray,
             ),
         );
+
+        setStartYmd(
+            new Date(
+                dayjs(endYmd).subtract(period, 'days').format('YYYY-MM-DD'),
+            ),
+        );
     };
 
     const onSearchButtonClick = () => {
-        const selectedTab = pipe(
-            periodTab,
-            filter((a) => a.isSelected),
-            head,
-        );
-
-        if (selectedTab) {
-            setSearchCondition((prev) => ({
-                ...prev,
-                startYmd: dayjs()
-                    .subtract(selectedTab.period, 'days')
-                    .format('YYYY-MM-DD'),
-            }));
-        }
+        setSearchCondition((prev) => ({
+            ...prev,
+            startYmd: dayjs(startYmd).format('YYYY-MM-DD'),
+            endYmd: dayjs(endYmd).format('YYYY-MM-DD'),
+        }));
     };
 
     return (
-        <SearchPeriodWrapper>
-            <PeriodWrapper>
+        <SearchPeriodContainer>
+            <PeriodTabContainer>
                 {periodTab.map(({ period, name, isSelected }) => (
                     <PeriodButton
                         key={period}
@@ -103,20 +120,155 @@ const SearchPeriod: FC<SearchPeriodProps> = ({
                         {name}
                     </PeriodButton>
                 ))}
-            </PeriodWrapper>
-            <PeriodWrapper>
-                <InputWithCalendarIcon
-                    type='before'
-                    value={dayjs(startYmd).format('YYYY-MM-DD')}
-                />
-                ~
-                <InputWithCalendarIcon
-                    type='after'
-                    value={dayjs(endYmd).format('YYYY-MM-DD')}
-                />
+            </PeriodTabContainer>
+
+            <PeriodContainer>
+                <DatePickerContainer>
+                    {/* TODO: Exclude dates (오늘날짜 이전) */}
+                    <ReactDatePicker
+                        locale={ko}
+                        dateFormat='yyyy.MM.dd'
+                        closeOnScroll={true}
+                        selected={startYmd}
+                        onChange={(date) => {
+                            if (date) {
+                                setStartYmd(date);
+                            }
+                        }}
+                        customInput={<InputWithCalendarIcon type='before' />}
+                        renderCustomHeader={({
+                            monthDate,
+                            customHeaderCount,
+                            decreaseMonth,
+                            increaseMonth,
+                        }) => {
+                            return (
+                                <div>
+                                    <button
+                                        aria-label='Previous Month'
+                                        className={
+                                            'react-datepicker__navigation react-datepicker__navigation--previous'
+                                        }
+                                        style={
+                                            customHeaderCount === 1
+                                                ? { visibility: 'hidden' }
+                                                : { visibility: 'visible' }
+                                        }
+                                        onClick={decreaseMonth}
+                                    >
+                                        <span
+                                            className={
+                                                'react-datepicker__navigation-icon react-datepicker__navigation-icon--previous'
+                                            }
+                                        >
+                                            {'<'}
+                                        </span>
+                                    </button>
+                                    <span className='react-datepicker__current-month'>
+                                        {monthDate.toLocaleString('ko-KR', {
+                                            month: 'long',
+                                            year: 'numeric',
+                                        })}
+                                    </span>
+                                    <button
+                                        aria-label='Next Month'
+                                        className={
+                                            'react-datepicker__navigation react-datepicker__navigation--next'
+                                        }
+                                        style={
+                                            customHeaderCount === 0
+                                                ? { visibility: 'hidden' }
+                                                : { visibility: 'visible' }
+                                        }
+                                        onClick={increaseMonth}
+                                    >
+                                        <span
+                                            className={
+                                                'react-datepicker__navigation-icon react-datepicker__navigation-icon--next'
+                                            }
+                                        >
+                                            {'>'}
+                                        </span>
+                                    </button>
+                                </div>
+                            );
+                        }}
+                    />
+                    <span style={{ padding: '0 10px' }}>~</span>
+                    {/* TODO: Exclude dates (startDt이전) */}
+                    <ReactDatePicker
+                        locale={ko}
+                        dateFormat='yyyy.MM.dd'
+                        closeOnScroll={true}
+                        selected={endYmd}
+                        onChange={(date) => {
+                            if (date) {
+                                setEndYmd(date);
+                            }
+                        }}
+                        customInput={<InputWithCalendarIcon type='after' />}
+                        renderCustomHeader={({
+                            monthDate,
+                            customHeaderCount,
+                            decreaseMonth,
+                            increaseMonth,
+                        }) => {
+                            return (
+                                <div>
+                                    <button
+                                        aria-label='Previous Month'
+                                        className={
+                                            'react-datepicker__navigation react-datepicker__navigation--previous'
+                                        }
+                                        style={
+                                            customHeaderCount === 1
+                                                ? { visibility: 'hidden' }
+                                                : { visibility: 'visible' }
+                                        }
+                                        onClick={decreaseMonth}
+                                    >
+                                        <span
+                                            className={
+                                                'react-datepicker__navigation-icon react-datepicker__navigation-icon--previous'
+                                            }
+                                        >
+                                            {'<'}
+                                        </span>
+                                    </button>
+                                    <span className='react-datepicker__current-month'>
+                                        {monthDate.toLocaleString('ko-KR', {
+                                            month: 'long',
+                                            year: 'numeric',
+                                        })}
+                                    </span>
+                                    <button
+                                        aria-label='Next Month'
+                                        className={
+                                            'react-datepicker__navigation react-datepicker__navigation--next'
+                                        }
+                                        style={
+                                            customHeaderCount === 0
+                                                ? { visibility: 'hidden' }
+                                                : { visibility: 'visible' }
+                                        }
+                                        onClick={increaseMonth}
+                                    >
+                                        <span
+                                            className={
+                                                'react-datepicker__navigation-icon react-datepicker__navigation-icon--next'
+                                            }
+                                        >
+                                            {'>'}
+                                        </span>
+                                    </button>
+                                </div>
+                            );
+                        }}
+                    />
+                </DatePickerContainer>
                 <SearchButton onClick={onSearchButtonClick}>조회</SearchButton>
-            </PeriodWrapper>
-        </SearchPeriodWrapper>
+            </PeriodContainer>
+        </SearchPeriodContainer>
     );
 };
 
