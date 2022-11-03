c312a101742ee79b4c794408e7f84d7d6311cf6d
jerome <jerome@geekstudio.co.kr>
2022-11-03 14:39:35 +0900

    :hammer: refactor: 클레임 관련 타입 수정 & 주석 추가

--- src/api/claim/member.ts
+++ src/api/claim/member.ts
@@ -1,43 +1,64 @@
 import { AxiosResponse } from 'axios';
+import dayjs from 'dayjs';
 
 import request, { defaultHeaders } from 'api/core';
 import { CLAIM_TYPE } from 'models';
 import {
-    CanceledPrice,
-    CancelOptions,
+    GetEstimatedRefundPriceBody,
+    CancelOptionsBody,
     ExchangeRequest,
-    ReturnAccount,
+    MemberClaim,
+    RefundAccountInfo,
     ReturnOption,
 } from 'models/claim';
+import { tokenStorage } from 'utils/storage';
+
+interface GetClaimListParams extends Paging, SearchDate {
+    claimTypes: Nullable<CLAIM_TYPE>;
+}
 
 const member = {
-    getClaimList: ({
-        claimTypes,
-        endYmd,
-        hasTotalCount,
-        pageNumber,
-        pageSize,
-        startYmd,
-    }: Paging &
-        SearchDate & { claimTypes?: CLAIM_TYPE }): Promise<AxiosResponse> =>
-        request({
+    /**
+     * 회원 클레임 목록 조회하기
+     *  - 클레임 목록을 조회하는 API입니다.
+     */
+    getClaimList: (
+        params: GetClaimListParams = {
+            claimTypes: null,
+            pageNumber: 1,
+            pageSize: 10,
+            hasTotalCount: false,
+            startYmd: dayjs().subtract(3, 'months').format('YYYY-MM-DD'),
+            endYmd: dayjs().format('YYYY-MM-DD'),
+        },
+    ): Promise<AxiosResponse<ItemList<MemberClaim>>> => {
+        const accessTokenInfo = tokenStorage.getAccessToken();
+
+        return request({
             method: 'GET',
             url: '/profile/claims',
             params: {
-                claimTypes,
-                endYmd,
-                hasTotalCount,
-                pageNumber,
-                pageSize,
-                startYmd,
+                claimTypes: params.claimTypes,
+                endYmd: params.endYmd,
+                hasTotalCount: params.hasTotalCount,
+                pageNumber: params.pageNumber,
+                pageSize: params.pageSize,
+                startYmd: params.startYmd,
             },
             headers: Object.assign({}, defaultHeaders(), {
-                accessToken: localStorage.getItem('accessToken') || '',
+                accessToken: accessTokenInfo?.accessToken || '',
             }),
-        }),
+        });
+    },
 
-    // TODO 이하 함수들 404 error message: 리소스(order option)를 찾지 못하였습니다, 혹은 orderNo 등 모름
-    requestCancelOptions: ({
+    /**
+     * 회원 옵션취소 신청하기(복수옵션)
+     *  - 다수의 선택옵션을 취소신청하는 API입니다.
+     *
+     * @param param0
+     * @returns
+     */
+    requestCancelOptionsBody: ({
         claimReasonDetail,
         responsibleObjectType,
         claimType,
@@ -46,8 +67,10 @@ const member = {
         bankAccountInfo,
         claimReasonType,
         refundsImmediately,
-    }: CancelOptions): Promise<AxiosResponse> =>
-        request({
+    }: CancelOptionsBody): Promise<AxiosResponse> => {
+        const accessTokenInfo = tokenStorage.getAccessToken();
+
+        return request({
             method: 'POST',
             url: '/profile/claims/cancel',
             data: {
@@ -61,34 +84,44 @@ const member = {
                 refundsImmediately,
             },
             headers: Object.assign({}, defaultHeaders(), {
-                accessToken: localStorage.getItem('accessToken') || '',
+                accessToken: accessTokenInfo?.accessToken || '',
             }),
-        }),
+        });
+    },
 
-    getRefundPrice: ({
-        claimType,
-        productCnt,
-        claimReasonType,
-        returnWayType,
-        claimedProductOptions,
-        responsibleObjectType,
-    }: CanceledPrice): Promise<AxiosResponse> =>
-        request({
+    /**
+     * 회원 클레임시 변경되는 주문의 환불 예상금액 계산하기(복수옵션)
+     *  - 다수의 선택옵션을 취소할 경우 환불 예상금액을 미리 계산하는 API입니다.
+     *
+     * @param param0
+     * @returns
+     */
+    getEstimatedRefundPrice: (
+        body: GetEstimatedRefundPriceBody,
+    ): Promise<AxiosResponse> => {
+        const accessTokenInfo = tokenStorage.getAccessToken();
+
+        return request({
             method: 'POST',
             url: '/profile/claims/estimate',
             data: {
-                claimType,
-                productCnt,
-                claimReasonType,
-                returnWayType,
-                claimedProductOptions,
-                responsibleObjectType,
+                claimType: body.claimType,
+                productCnt: body.productCnt,
+                claimReasonType: body.claimReasonType,
+                returnWayType: body.returnWayType,
+                claimedProductOptions: body.claimedProductOptions,
+                responsibleObjectType: body.responsibleObjectType,
             },
             headers: Object.assign({}, defaultHeaders(), {
-                accessToken: localStorage.getItem('accessToken') || '',
+                accessToken: accessTokenInfo?.accessToken || '',
             }),
-        }),
+        });
+    },
 
+    /**
+     * 회원 반품 신청하기(복수옵션)
+     *  - 다수의 선택옵션을 반품하는 API입니다.
+     */
     requestReturnOfMultipleOptions: ({
         claimImageUrls,
         claimType,
@@ -103,8 +136,10 @@ const member = {
         deliveryCompanyType,
         invoiceNo,
         responsibleObjectType,
-    }: ReturnOption & CanceledPrice): Promise<AxiosResponse> =>
-        request({
+    }: ReturnOption & GetEstimatedRefundPriceBody): Promise<AxiosResponse> => {
+        const accessTokenInfo = tokenStorage.getAccessToken();
+
+        return request({
             method: 'POST',
             url: '/profile/claims/return',
             data: {
@@ -123,22 +158,38 @@ const member = {
                 responsibleObjectType,
             },
             headers: Object.assign({}, defaultHeaders(), {
-                accessToken: localStorage.getItem('accessToken') || '',
+                accessToken: accessTokenInfo?.accessToken || '',
             }),
-        }),
+        });
+    },
 
+    /**
+     * 회원 환불 계좌 정보 수정하기
+     *  - 환불 계좌 정보를 수정하는 API입니다.
+     *
+     * @param claimNo
+     * @param param1
+     * @returns
+     */
     updateReturnAccount: (
         claimNo: string,
-        { bank, account, depositorName }: ReturnAccount,
-    ): Promise<AxiosResponse> =>
-        request({
+        body: RefundAccountInfo,
+    ): Promise<AxiosResponse> => {
+        const accessTokenInfo = tokenStorage.getAccessToken();
+
+        return request({
             method: 'PUT',
             url: `/profile/claims/${claimNo}/account`,
-            data: { bank, account, depositorName },
+            data: {
+                bank: body.bank,
+                account: body.account,
+                depositorName: body.depositorName,
+            },
             headers: Object.assign({}, defaultHeaders(), {
-                accessToken: localStorage.getItem('accessToken') || '',
+                accessToken: accessTokenInfo?.accessToken || '',
             }),
-        }),
+        });
+    },
 
     checkClaimValidation: (claimNo: string): Promise<AxiosResponse> =>
         request({
@@ -149,15 +200,24 @@ const member = {
             }),
         }),
 
-    getClaimDetailByClaimNo: (claimNo: string): Promise<AxiosResponse> =>
-        request({
+    /**
+     * 회원 클레임 상세보기(클레임 번호)
+     *  - 클레임 번호로 클레임 세부 내역을 조회합니다.
+     *
+     * @param claimNo
+     * @returns
+     */
+    getClaimDetailByClaimNo: (claimNo: string): Promise<AxiosResponse> => {
+        const accessTokenInfo = tokenStorage.getAccessToken();
+
+        return request({
             method: 'GET',
             url: `/profile/claims/${claimNo}/result`,
             headers: Object.assign({}, defaultHeaders(), {
-                accessToken: localStorage.getItem('accessToken') || '',
+                accessToken: accessTokenInfo?.accessToken || '',
             }),
-        }),
-
+        });
+    },
     cancelClaimByClaimNo: (claimNo: string): Promise<AxiosResponse> =>
         request({
             method: 'PUT',
@@ -191,7 +251,7 @@ const member = {
             claimReasonType,
             refundsImmediately,
             productCnt,
-        }: Omit<CancelOptions, 'claimedProductOptions'> & {
+        }: Omit<CancelOptionsBody, 'claimedProductOptions'> & {
             productCnt: number;
         },
     ): Promise<AxiosResponse> =>
@@ -221,7 +281,7 @@ const member = {
             productCnt,
             claimReasonType,
             returnWayType,
-        }: Omit<CanceledPrice, 'claimedProductOptions'>,
+        }: Omit<GetEstimatedRefundPriceBody, 'claimedProductOptions'>,
     ): Promise<AxiosResponse> =>
         request({
             method: 'GET',
@@ -255,7 +315,7 @@ const member = {
             invoiceNo,
             exchangeAddress,
             exchangeOption,
-        }: Omit<CanceledPrice, 'claimedProductOptions'> &
+        }: Omit<GetEstimatedRefundPriceBody, 'claimedProductOptions'> &
             ReturnOption &
             ExchangeRequest,
     ): Promise<AxiosResponse> =>
@@ -309,7 +369,8 @@ const member = {
             deliveryCompanyType,
             invoiceNo,
             responsibleObjectType,
-        }: ReturnOption & Omit<CanceledPrice, 'claimedProductOptions'>,
+        }: ReturnOption &
+            Omit<GetEstimatedRefundPriceBody, 'claimedProductOptions'>,
     ): Promise<AxiosResponse> =>
         request({
             method: 'POST',
@@ -354,7 +415,7 @@ const member = {
             bankAccountInfo,
             claimReasonType,
             refundsImmediately,
-        }: Omit<CancelOptions, 'claimedProductOptions'>,
+        }: Omit<CancelOptionsBody, 'claimedProductOptions'>,
     ): Promise<AxiosResponse> =>
         request({
             method: 'POST',
--- src/models/claim/index.ts
+++ src/models/claim/index.ts
@@ -5,33 +5,61 @@ import {
     RETURN_WAY_TYPE,
     DELIVERY_COMPANY_TYPE,
     BANK,
+    REFUND_TYPE,
+    CLAIM_CLASS_TYPE,
+    PAY_TYPE,
+    PG_TYPE,
 } from 'models';
+import {
+    DeliveryAmtInfo,
+    OrderOption,
+    ProductAmtInfo,
+    ReturnAddress,
+} from 'models/order';
 
+// 계좌정보
 interface BankAccountInfo {
+    // 계좌번호 (nullable) (example: 1002134134983)
     bankAccount?: string;
+    // 예금주명 (nullable) (example: 홍길동)
     bankDepositorName?: string;
-    bank: BANK;
+    // 은행 코드 (nullable) (example: KDB)
+    bank?: BANK;
+    // 은행명 (nullable) (example: 산업은행)
     bankName?: string;
 }
 
-interface ReturnAddress {
-    addressNo: number;
-    receiverName: string;
-    addressName: string;
-    receiverContact1: string;
-    receiverContact2?: string;
-    receiverZipCd: string;
-    receiverAddress: string;
-    receiverJibunAddress: string;
-    receiverDetailAddress: string;
-    customsIdNumber: string;
-    countryCd: string;
-    deliveryMemo: string;
+// 환불 계좌 정보
+export interface RefundAccountInfo {
+    // 계좌소유자명 (nullable, optional) example: 홍길동
+    depositorName?: string;
+    // 은행코드 (nullable, optional) (example: KB)
+    bank?: BANK;
+    // 계좌번호 (nullable, optional) (example: 123-12-1234)
+    account?: string;
 }
 
+// interface ReturnAddress {
+//     addressNo: number;
+//     receiverName: string;
+//     addressName: string;
+//     receiverContact1: string;
+//     receiverContact2?: string;
+//     receiverZipCd: string;
+//     receiverAddress: string;
+//     receiverJibunAddress: string;
+//     receiverDetailAddress: string;
+//     customsIdNumber: string;
+//     countryCd: string;
+//     deliveryMemo: string;
+// }
+
+// 주문상품옵션정보
 interface ClaimedProductOption {
-    orderProductOptionNo: number;
+    // 취소/반품할 제품수량 (example: 1)
     productCnt: number;
+    // 주문 상품 옵선 번호 (example: 1)
+    orderProductOptionNo: number;
 }
 
 interface InputText {
@@ -46,24 +74,41 @@ interface ExchangeOption {
     productNo: number;
     additionalProductNo: number;
 }
+type ResponsibleObjectType = null extends RESPONSIBLE_OBJECT_TYPE
+    ? CLAIM_REASON_TYPE
+    : RESPONSIBLE_OBJECT_TYPE;
 
-export interface CancelOptions {
+export interface CancelOptionsBody {
+    // 상세사유 (example: 다른상품구매)
     claimReasonDetail: string;
-    responsibleObjectType: RESPONSIBLE_OBJECT_TYPE;
+    // 귀책 - responsibleObjectType이 null이면 ClaimReasonType에 매핑되는 귀책 적용 (nullable) (example: BUYER)
+    responsibleObjectType: ResponsibleObjectType;
+    // 클레임타입 (example: CANCEL)
     claimType: Omit<CLAIM_TYPE, 'NONE'>;
+    // 주문상품옵션정보
     claimedProductOptions: ClaimedProductOption[];
-    saveBankAccountInfo?: boolean;
+    // 환불계좌정보 저장 여부 (example: true)
+    saveBankAccountInfo: boolean;
+    // 계좌정보 (nullable)
     bankAccountInfo?: BankAccountInfo;
+    // 클레임사유 (example: CHANGE_MIND)
     claimReasonType: CLAIM_REASON_TYPE;
+    // 즉시환불여부(기본 값: true)(서비스 플랜이 프리미엄이고, 주문상태가 결제완료인 옵션인 경우 즉시환불 가능) (example: true)
     refundsImmediately: boolean;
 }
 
-export interface CanceledPrice {
-    responsibleObjectType?: RESPONSIBLE_OBJECT_TYPE;
+export interface GetEstimatedRefundPriceBody {
+    // 귀책 - responsibleObjectType이 null이면 ClaimReasonType에 매핑되는 귀책 적용 (nullable) (example: BUYER)
+    responsibleObjectType: ResponsibleObjectType;
+    // 주문상품옵션정보
     claimedProductOptions: ClaimedProductOption[];
+    // 클레임타입 (example: CANCEL)
     claimType: Omit<CLAIM_TYPE, 'NONE'>;
+    // 취소/반품할 제품수량
     productCnt: number;
+    // 클레임사유 (example: CHANGE_MIND)
     claimReasonType: CLAIM_REASON_TYPE;
+    // 반품상품 수거방법 (nullable) (example: SELLER_COLLECT)
     returnWayType?: RETURN_WAY_TYPE;
 }
 
@@ -77,15 +122,159 @@ export interface ReturnOption {
     invoiceNo?: string;
 }
 
-export interface ReturnAccount {
-    depositorName?: string;
-    bank?: BANK;
-    account?: string;
-}
-
 export interface ExchangeRequest {
     exchangeAddress: Omit<ReturnAddress, 'addressNo' | 'addressName'> & {
         addressView: string;
     };
     exchangeOption: ExchangeOption;
+}
+
+export interface MemberClaim {
+    // 클레임사유상세 (nullable) (example: 변심)
+    claimReasonDetail: Nullable<string>;
+    // 취소/반품상품상품들
+    claimedProductOptions: ClaimedProductOption[];
+    // 클레임타입 (example: CANCEL)
+    claimType: Omit<CLAIM_TYPE, 'NONE'>;
+    // 클레임번호 (example: 1)
+    claimNo: number;
+    // 주문번호 (example: 1)
+    orderNo: string;
+    // 클레임사유 (example: CHANGE_MIND)
+    claimReasonType: CLAIM_REASON_TYPE;
+    // 주문일자 (example: YYYY-MM-DD hh:mm:ss)
+    orderYmdt: string;
+    // 클레임신청일시 (example: YYYY-MM-DD hh:mm:ss)
+    claimYmdt: string;
+    // 클레임금액정보 (nullable)
+    claimPriceInfo: Nullable<ClaimPriceInfo>;
+}
+
+// 금액정보
+export interface ClaimPriceInfo {
+    // 차감금액정보
+    subtractionAmtInfo: {
+        // 총주문차감금액
+        totalAmt: number;
+
+        // 배송비쿠폰 변경금액 (example: 0)
+        deliveryCouponAmt: number;
+
+        // 환불금액조정사유 (nullable) (example: 오배송)
+        refundAdjustReason: string;
+
+        // 환불금액조정 (example: 0)
+        refundAdjustAmt: number;
+
+        // 장바구니쿠폰 변경금액 (example: 0)
+        cartCouponAmt: number;
+    };
+    // 환불방법(노출용) (example: 신용카드)
+    refundTypeLabel: string;
+    // 환불금액(적립금제외) (example: 1000)
+    refundMainPayAmt: number;
+    // 환불방법 (nullable) (example: CANCEL_DEPOSIT), TODO: omit
+    refundType: Nullable<REFUND_TYPE>;
+    // 배송비정보
+    deliveryAmtInfo: ClaimDeliveyAmtInfo;
+    // 적립금환불금액 (example: 1000)
+    refundSubPayAmt: number;
+    // 환불금액(적립금포함) (example: 1000)
+    refundPayAmt: number;
+    // 추가결제금액 (example: 1000)
+    additionalPayAmt: number;
+    // 환불결제방법 (nullable) (example: CREDIT_CARD)
+    refundPayType: Nullable<REFUND_TYPE>;
+    // 상품금액정보, TODO: models/order/index.ts 의 productAmyInfo와 뭐가 다른지 체크
+    productAmtInfo: ClaimProductAmtInfo;
+}
+
+export interface ClaimDeliveyAmtInfo extends DeliveryAmtInfo {
+    // 변경 전 지역별 배송비 (example: 0)
+    beforeRemoteDeliveryAmt: number;
+    // 변경 후 지역별 배송비 (example: 0)
+    afterRemoteDeliveryAmt: number;
+}
+
+export interface ClaimProductAmtInfo
+    extends Omit<ProductAmtInfo, 'returnImmediateDiscountedPrice'> {
+    // 사은품할인금액 (example: 0)
+    freeGiftDiscountAmt: number;
+}
+
+export interface GetClaimDetailByClaimNoResponse {
+    // 클레임사유상세 (nullable) (example: 다른상품구매)
+    claimReasonDetail: Nullable<string>;
+    // 반품배송정보 (nullable)
+    returnDelivery: Nullable<{
+        // 택배사명 (nullable) (example: CJ대한통운)
+        deliveryCompanyTypeLabel: string;
+        // 택배사타입 (nullable) (example: CJ)
+        deliveryCompanyType: Nullable<DELIVERY_COMPANY_TYPE>;
+        // 송장번호 (nullable) (example: 123455)
+        invoiceNo: Nullable<string>;
+        // 무적반품(임의반품) 여부 (example: false)
+        evadesReturnProcess: boolean;
+    }>;
+    // claimedOptions	[...]
+
+    // 클레임번호 (example: 1)
+    claimNo: number;
+    // exchangedOption	{...}
+
+    // 클레임사유 (example: CHANGE_MIND)
+    claimReasonType: CLAIM_REASON_TYPE;
+    // 반품상품 수거방법 (nullable) (example: SELLER_COLLECT)
+    returnWayType: Nullable<RETURN_WAY_TYPE>;
+    // 주문옵션번호 (example: 1)
+    orderProductOptionNo: number;
+    // 클레임신청일시 (example: YYYY-MM-DD hh:mm:ss)
+    claimYmdt: string;
+    // 교환 추가금액 결제 정보 (nullable)
+    exchangePayInfo: Nullable<ExchangePayInfo>;
+    // 출고전교환인경우 true (example: true)
+    exchangeBeforeDelivery: boolean;
+    // 반품/교환 이미지 첨부파일 url 리스트 (nullable) (example: url1,url2)
+    claimImageUrls: string[];
+    // claimedOption	{...}
+
+    // 반품수거지 (nullable)
+    returnAddress: Nullable<Omit<ReturnAddress, 'customsIdNumber'>>;
+    // 환불방법 (nullable) (example: CANCEL_DEPOSIT)
+    refundType: REFUND_TYPE;
+    // 클레임타입 (example: CANCEL)
+    claimType: Omit<CLAIM_TYPE, 'NONE'>;
+    // 교환배송지 (nullable)
+    exchangeAddress: Nullable<Omit<ReturnAddress, 'customsIdNumber'>>;
+    // 클레임종류 (example: OPTION_CANCEL)
+    claimClassType: CLAIM_CLASS_TYPE;
+    // 금액정보
+    claimPriceInfo: ClaimPriceInfo;
+    // 환불계좌 정보(무통장 및 가상계좌) (nullable)
+    refundBankAccount: Nullable<RefundAccountInfo>;
+}
+
+// 교환 추가금액 결제 정보
+export interface ExchangePayInfo {
+    // 교환금액 (example: 10000)
+    exchangePayAmt: number;
+    // 계좌정보 (nullable)
+    bankAccount: Nullable<BankAccountInfo>;
+    // 결제방법 (nullable) (example: CASH)
+    payType: Nullable<'CASH' | 'ACCUMULATION' | 'NAVER_PAY'>;
+    // 입금자 (nullable) (example: 홍길동)
+    remitter: Nullable<string>;
+}
+
+export interface ClaimOrderOption extends OrderOption {
+    // 클레임사유 (example: CHANGE_MIND)
+    claimReasonType: CLAIM_REASON_TYPE;
+    // 수량할인 여부
+    isQuantityDiscount: boolean;
+    // 결제수단 (example: CREDIT_CARD)
+    payType: string;
+    // 상세사유 (nullable) (example: 상세사유)
+    claimReasonDetail: Nullable<string>;
+    // PG타입 (example: PAYCO)
+    pgType: PG_TYPE;
 }
