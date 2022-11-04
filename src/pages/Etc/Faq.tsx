8b24d42b1b8d30342f550c46eaa17a0714484ff3
George <george@geekstudio.co.kr>
2022-11-04 15:55:21 +0900

    :art: chore: 태그 수정

--- src/pages/Etc/Faq.tsx
+++ src/pages/Etc/Faq.tsx
@@ -582,7 +582,7 @@ const Faq = () => {
                                                             dangerouslySetInnerHTML={{
                                                                 __html: content!,
                                                             }}
-                                                        ></FaqDetail>
+                                                        />
                                                     </SectionDropdown>
                                                 </FaqDetailBox>
                                             );
