9ecc3a34088323aab3c7a6ff59b0a44363cc8e4c
jerome <jerome@geekstudio.co.kr>
2022-11-09 13:16:20 +0900

    :sparkles: feat: 라운드 분석 > 통계 탭에 라인 차트 추가

--- src/pages/MyPage/RoundAnyalysis/Statistics.tsx
+++ src/pages/MyPage/RoundAnyalysis/Statistics.tsx
@@ -1,8 +1,17 @@
 import { FC, useState, useLayoutEffect, useMemo } from 'react';
-import styled from 'styled-components';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
-import { isEmpty, map, pipe, toArray } from '@fxts/core';
+import styled, { css } from 'styled-components';
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+} from 'chart.js';
+import { Doughnut, Line } from 'react-chartjs-2';
+import { filter, head, isEmpty, map, pipe, toArray } from '@fxts/core';
 
 import media from 'utils/styles/media';
 import { flex } from 'utils/styles/mixin';
@@ -12,7 +21,15 @@ interface StatisticsProps {
     isMainTab?: boolean;
 }
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+);
 
 const StyledContainer = styled.section`
     margin-bottom: 20px;
@@ -40,6 +57,7 @@ const Title = styled.h3`
 const StatisticContainer = styled.div`
     background-color: #f8f8fa;
     display: flex;
+    margin-bottom: 20px;
 
     ${media.medium} {
         flex-direction: column-reverse;
@@ -85,7 +103,88 @@ const ChartContainer = styled.div`
     position: relative;
 `;
 
-const Statistics: FC<StatisticsProps> = ({ isMainTab = true }) => {
+const SearchTabList = styled.ul`
+    ${flex}
+    justify-content: flex-start;
+    margin-bottom: 10px;
+`;
+
+const SearchTabListItem = styled.li<{ isSelected?: boolean }>`
+    margin-right: 10px;
+    font-size: 12px;
+    line-height: 18px;
+    letter-spacing: -0.48px;
+    padding: 6px 0;
+    width: 88px;
+    text-align: center;
+
+    ${(props) =>
+        props.isSelected
+            ? css`
+                  background-color: #222943;
+                  color: #fff;
+              `
+            : css`
+                  background-color: #f0eff4;
+                  color: #222943;
+              `};
+`;
+
+const BestScore = styled.div`
+    background-color: #f8f8fa;
+    text-align: center;
+    width: 100%;
+    ${flex}
+    flex-direction: column;
+
+    p {
+        font-size: 24px;
+        line-height: 36px;
+        letter-spacing: 0;
+        padding: 20px;
+        margin-bottom: 20px;
+    }
+`;
+
+const ScoreFilterItem = styled.ul`
+    ${flex}
+    justify-content: flex-start;
+    margin-bottom: 20px;
+`;
+
+const ScoreFilterItemList = styled.li<{ isSelected?: boolean }>`
+    margin-right: 40px;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.64px;
+
+    ${(props) =>
+        props.isSelected
+            ? css`
+                  color: #191919;
+                  text-decoration: underline;
+              `
+            : css`
+                  color: #a8a8a8;
+              `};
+`;
+
+const Statistics: FC<StatisticsProps> = ({ isMainTab = false }) => {
+    const [searchCondition, setSearchCondition] = useState([
+        { id: 'all', name: '전체', isSelected: true },
+        { id: '5round', name: '5라운드', isSelected: false },
+        { id: '10round', name: '10라운드', isSelected: false },
+        { id: 'period', name: '기간선택', isSelected: false },
+    ]);
+
+    const [scoreFilter, setScoreFilter] = useState([
+        { id: 'score', name: '스코어', isSelected: true },
+        { id: 'gir', name: 'GIR', isSelected: false },
+        { id: 'putt', name: '퍼트수', isSelected: false },
+        { id: 'distance', name: '비거리', isSelected: false },
+    ]);
+
     const [chartData, setChartData] = useState([
         { id: 'buddy', name: '버디', value: 100 },
         { id: 'par', name: '파', value: 0 },
@@ -140,12 +239,66 @@ const Statistics: FC<StatisticsProps> = ({ isMainTab = true }) => {
         [chartData, hasProduct],
     );
 
+    const selectedSearchCondition = useMemo(
+        () =>
+            pipe(
+                searchCondition,
+                filter((a) => a.isSelected),
+                head,
+            ),
+        [searchCondition],
+    );
+
+    const onSearchTabClick = (name: string, id: string) => {
+        if (name === 'searchCondition') {
+            setSearchCondition((prev) =>
+                pipe(
+                    prev,
+                    map((a) => ({ ...a, isSelected: a.id === id })),
+                    toArray,
+                ),
+            );
+        }
+
+        if (name === 'scoreFilter') {
+            setScoreFilter((prev) =>
+                pipe(
+                    prev,
+                    map((a) => ({ ...a, isSelected: a.id === id })),
+                    toArray,
+                ),
+            );
+        }
+    };
+
     return (
         <StyledContainer>
             <Title>
                 통계
-                <span>{`(최근 ${roundStats.data.roundStatistics.gameCount}라운드)`}</span>
+                {/* TODO 메인 탭을 눌렀을 경우에도 해당 값이 true가 되어야함 */}
+                {isMainTab && (
+                    <span>{`(최근 ${roundStats.data.roundStatistics.gameCount}라운드)`}</span>
+                )}
             </Title>
+
+            {!isMainTab && (
+                <SearchTabList>
+                    {searchCondition.map(({ id, name, isSelected }) => (
+                        <SearchTabListItem
+                            isSelected={isSelected}
+                            onClick={() =>
+                                onSearchTabClick('searchCondition', id)
+                            }
+                        >
+                            {name}
+                        </SearchTabListItem>
+                    ))}
+                    {selectedSearchCondition?.id === 'period' && (
+                        <div>날짜선택</div>
+                    )}
+                </SearchTabList>
+            )}
+
             <StatisticContainer>
                 <StatisticSummary>
                     <div>
@@ -277,6 +430,66 @@ const Statistics: FC<StatisticsProps> = ({ isMainTab = true }) => {
                     />
                 </ChartContainer>
             </StatisticContainer>
+
+            {!isMainTab && (
+                <div>
+                    <ScoreFilterItem>
+                        {scoreFilter.map(({ id, name, isSelected }) => (
+                            <ScoreFilterItemList
+                                key={id}
+                                isSelected={isSelected}
+                                onClick={() =>
+                                    onSearchTabClick('scoreFilter', id)
+                                }
+                            >
+                                {name}
+                            </ScoreFilterItemList>
+                        ))}
+                    </ScoreFilterItem>
+
+                    <BestScore>
+                        {/* TODO: 다국어 처리(https://react.i18next.com/latest/trans-component 참고) */}
+                        <p>
+                            홍길동님의 최신 <b>베스트 스코어</b>는<br />
+                            <b>22-07-14 동원 로얄 CC</b> 에서의{' '}
+                            <b style={{ color: '#508CFE' }}>73</b> 입니다.
+                        </p>
+                        <div style={{ width: '100%' }}>
+                            <Line
+                                options={{
+                                    responsive: true,
+                                    backgroundColor: '#fff',
+                                    plugins: {
+                                        legend: {
+                                            display: false,
+                                        },
+                                    },
+                                }}
+                                data={{
+                                    labels: [
+                                        '07-01',
+                                        '07-04',
+                                        '07-05',
+                                        '07-09',
+                                        '07-14',
+                                        '08-01',
+                                        '09-01',
+                                    ],
+                                    datasets: [
+                                        {
+                                            label: 'My First Dataset',
+                                            data: [65, 59, 80, 81, 56, 55, 40],
+                                            fill: false,
+                                            borderColor: '#707070',
+                                            tension: 0.1,
+                                        },
+                                    ],
+                                }}
+                            />
+                        </div>
+                    </BestScore>
+                </div>
+            )}
         </StyledContainer>
     );
 };
