49c9eefd4c6bc02bd7c6c657b757dc5c44e66324
jerome <jerome@geekstudio.co.kr>
2022-12-01 12:08:05 +0900

    :sparkles: feat: swagger 추가

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const app = express();
+
+const port = 80; //인스턴스 생성시 만들었던 포트번호 기입
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.set({
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        Pragma: 'no-cache',
+        Date: Date.now(),
+    });
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+http.createServer(app).listen(port, () => {
+    console.log(`app listening at ${port}`);
+});
